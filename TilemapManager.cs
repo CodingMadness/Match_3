c72ad58e563d70cc9ab6d1c1bb478a978c641e38
Lazarus <shho310795@proton.me>
2022-09-11 22:11:28 +0200

    ADDED these couple months ago created files for 2D Grid game-logic, very helpful

---
+++ TilemapManager.cs
@@ -0,0 +1,237 @@
+﻿//using DotNext;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using Raylib_cs;
+
+namespace Match_3
+{
+    public abstract class Grid
+    {
+        public const int FLOORTILE_WIDTH = 64;
+        public const int FLOORTILE_HEIGHT = 64;
+
+        public double Timer { get; private set; } = 10;
+        public readonly int _tileCountInX;
+        public readonly int _tileCountInY;
+        protected readonly Tile[,] _bitmap;
+        protected readonly Texture2D _image;
+
+        private bool stopDrawingSameStuff = false;
+
+        protected Grid(int tileCountInX, int tileCountInY, Texture2D img)
+        {
+            _tileCountInX = tileCountInX;
+            _tileCountInY = tileCountInY;
+            _bitmap = new Tile[_tileCountInX, _tileCountInY];
+            _image = img;
+        }
+
+        protected abstract void internalDraw(SpriteBatch sb, Tile current, SpriteFont font);
+
+        public abstract void CreateMap();
+
+        public void Draw(SpriteBatch sb, GameTime deltaTime, SpriteFont font)
+        {
+            if (stopDrawingSameStuff)
+                return;
+            //–
+            Timer -= deltaTime.ElapsedGameTime.TotalSeconds;
+            // Debug.WriteLine("Timer: " + Timer);
+            const int timeMax = 10;//11 seconds for all tiles to appear
+
+            var p = (Timer / timeMax);
+            var px = p * _tileCountInX;
+            var py = p * _tileCountInY;
+
+            for (int x = 0; x < _tileCountInX; x++)
+            {
+                for (int y = 0; y < _tileCountInY; y++)
+                {
+                    //if (px < y && py < y)
+                    {
+                        var tile = _bitmap[x, y];
+
+                        if (tile != null)
+                        {
+                            internalDraw(sb, tile, font);
+                            //Debug.WriteLine("I DRAWED SMTH!");
+                        }
+                        //stopDrawingSameStuff = x == _tileCountInX-1 && y == _tileCountInY-1;
+                        //Debug.WriteLine(stopDrawingSameStuff +  "   :  " + "(" + x + "," + y + ")");
+                    }
+                }
+            }
+
+        }
+
+        public Tile this[int r, int c]
+        {
+            get
+            {
+                if (r >= 0 && c >= 0 && r < _tileCountInX && c < _tileCountInY)
+                {
+                    var tmp = _bitmap[r, c];
+                    return tmp ?? throw new IndexOutOfRangeException("");
+                }
+                return null;
+            }
+            set
+            {
+                if (r >= 0 && c >= 0 && r < _tileCountInX && c < _tileCountInY)
+                {
+                    _bitmap[r, c] = value;
+                }
+            }
+        }
+    }
+
+    public class BorderMap : Grid
+    {
+        public BorderMap(int width, int height, Texture2D single) : base(width, height, single)
+        {
+
+        }
+
+        public override void CreateMap()
+        {
+            //Here x fill the entire LEFT-ROW and RIGHT-ROW
+            int start = 0;
+            //_bitmap[spalte=0, zeile=y]
+            for (int x = 0; x < _tileCountInY; x++)
+            {
+                _bitmap[start, x] = Tile.CreateTile(0, start, x, BORDERTILE_WIDTH, BORDERTILE_HEIGHT, _image, Tile.TileKind.Border);
+
+                if (x == _tileCountInY - 1)
+                {
+                    x = -1;
+                    start = _tileCountInX - 1;
+
+                    if (_bitmap[start, x + 1] != null)
+                        break;
+                }
+            }
+
+            start = 0;
+            //_bitmap[spalte=0, zeile=y]
+            for (int x = 0; x < _tileCountInX; x++)
+            {
+                _bitmap[x, start] = Tile.CreateTile(0, x, start, BORDERTILE_WIDTH, BORDERTILE_HEIGHT, _image, Tile.TileKind.Border);
+
+                if (x == _tileCountInX - 1)
+                {
+                    x = -1;
+                    start = _tileCountInY - 1;
+
+                    if (_bitmap[x + 2, start] != null)
+                        break;
+                }
+            }
+        }
+
+        protected override void internalDraw(SpriteBatch sb, Tile current, SpriteFont font)
+        {
+            sb.Draw(current.TileImg, current.DrawableRect, Color.White);
+            //sb.DrawString(font, $"{current.Cell.X} {current.Cell.Y}", current.Cell, Color.Black);
+        }
+    }
+
+    public class FloorMap : Grid
+    {
+        public FloorMap(int width, int height, Texture2D single) : base(width, height, single)
+        {
+
+        }
+
+        public override void CreateMap()
+        {
+            for (int x = 1; x < (_tileCountInX); x++)
+            {
+                for (int y = 1; y < (_tileCountInX); y++)
+                {
+                    _bitmap[x, y] = Tile.CreateTile(1, x, y, FLOORTILE_WIDTH, FLOORTILE_HEIGHT, _image, Tile.TileKind.Floor);
+                }
+            }
+        }
+
+        protected override void internalDraw(SpriteBatch sb, Tile current, SpriteFont font)
+        {
+            sb.Draw(current.TileImg, current.Cell, Color.White);
+            sb.DrawString(font, $"{current.Cell.X} {current.Cell.Y}", current.Cell, Color.Red);
+        }
+    }
+
+    public class PickupsMap : Grid
+    {
+        private readonly FloorMap pickupGround;
+        private readonly WeightedCellPool cellPool;
+
+        private IEnumerable<Vector2> YieldGameWindow()
+        {
+            for (int x = 1; x < pickupGround._tileCountInX - 0; x++)
+            {
+                for (int y = 1; y < pickupGround._tileCountInY - 0; y++)
+                {
+                    Vector2 current = new(x * PICKUPTILE_WIDTH * 2, y * PICKUPTILE_HEIGHT * 2);
+                    //if (x % 2 == 0)
+                    yield return current;
+                }
+            }
+        }
+
+        public PickupsMap(in FloorMap groundForPickups, Texture2D sheet)
+            : base(groundForPickups._tileCountInX, groundForPickups._tileCountInY, sheet)
+        {
+            pickupGround = groundForPickups;
+            //cellPool = new(YieldGameWindow());
+        }
+
+        public override void CreateMap()
+        {
+            for (int x = 1; x < (_tileCountInX) - 1; x++)
+            {
+                Vector2 tmp = new(2 * FLOORTILE_WIDTH, 2 * FLOORTILE_HEIGHT);
+                _bitmap[1, 1] = Tile.CreateRndTileFromSheet(2, tmp/*pickupGround[1, 1].Cell*/, PICKUPTILE_WIDTH,
+                                                                PICKUPTILE_HEIGHT,
+                                                                _image, Tile.TileKind.Pickup);
+                //for (int y = 1; y < (_tileCountInX); y++)
+                //{
+                //    _bitmap[x, y] = Tile.CreateRndTileFromSheet(pickupGround[x,y].Cell, PICKUPTILE_WIDTH, 
+                //                                                PICKUPTILE_HEIGHT, 
+                //                                                _image, Tile.TileKind.Pickup);
+                //}
+            }
+        }
+
+        protected override void internalDraw(SpriteBatch sb, Tile current, SpriteFont font)
+        {
+            if (current.Cell != default)
+                sb.Draw(current.TileImg, current.Cell, current.DrawableRect, Color.White);
+            //WORKS! //sb.DrawString(font, $"{current.Cell.X} {current.Cell.Y}", current.Cell, Color.Red);
+        }      
+    }
+
+    //public sealed class CollisionPool : ICollidable
+    //{
+    //    readonly ICollidable[] _maps;
+
+    //    public CollisionPool(params ICollidable[] maps)
+    //    {
+    //        _maps = maps;
+    //    }
+
+    //    public bool CheckIfPlayerCollidesWithMe(in Rectangle srcToCheck, Direction dir)
+    //    {
+    //        for (int i = 0; i < _maps.Length; i++)
+    //        {
+    //            if (_maps[i].CheckIfPlayerCollidesWithMe(srcToCheck, dir))
+    //            {
+    //                return true;
+    //            }
+    //        }
+    //        return false;
+    //    }
+    //}
+}
---
+++ Timeing.cs
@@ -0,0 +1,264 @@
+﻿using System;
+
+namespace Microsoft.Xna.Framework
+{
+    /// <summary>
+    /// Timer class based on seconds.
+    /// The timer is a non zero based count up timer.
+    /// </summary>
+    public class Timeing
+    {
+        private float timer = 0f;
+        private float elapsedTime = 0f;
+        private float multiplier = 1f;
+        bool isTriggered = false;
+        const float pi = 3.141592653f;
+
+        public Timeing()
+        {
+            Timer = 1.0f;
+            IsActivelyTiming = false;
+            isTriggered = false;
+        }
+        public Timeing(float seconds)
+        {
+            Timer = seconds;
+            IsActivelyTiming = true;
+            isTriggered = false;
+        }
+        public Timeing(float seconds, bool startingStateActive)
+        {
+            Timer = seconds;
+            IsActivelyTiming = startingStateActive;
+            isTriggered = false;
+        }
+
+        public bool IsActivelyTiming { get; set; } = false;
+
+        /// <summary>
+        /// set or get the timer amount directly.
+        /// </summary>
+        public float Timer
+        {
+            set { timer = value; }
+            get { return timer; }
+        }
+
+        /// <summary>
+        /// set or get the elapsedTime from zero directly.
+        /// </summary>
+        public float Elapsed
+        {
+            set { elapsedTime = value; }
+            get { return elapsedTime; }
+        }
+
+        /// <summary>
+        /// This value multiplies the amount to add to the elapsed time when updated.
+        /// </summary>
+        public float Multiplier
+        {
+            set { multiplier = value; }
+            get { return multiplier; }
+        }
+
+        public float SetTimerAndStart
+        {
+            set
+            {
+                timer = value;
+                IsActivelyTiming = true;
+                isTriggered = false;
+            }
+        }
+
+        public float AddToTimer
+        {
+            set { timer += value; }
+        }
+        public float AddToElapsed
+        {
+            set { elapsedTime += value; }
+        }
+        public float AddToMultiplier
+        {
+            set { multiplier += value; }
+        }
+
+        public void StartTiming()
+        {
+            IsActivelyTiming = true;
+        }
+        public void StopTiming()
+        {
+            IsActivelyTiming = false;
+        }
+
+        /// <summary>
+        /// Set the elapsed to timer value if actively timing the timer will be triggered.
+        /// </summary>
+        public void TriggerTimer()
+        {
+            elapsedTime = timer;
+        }
+
+        /// <summary>
+        /// Leaves the timer running, retains the timing amount and restarts the timer at zero.
+        /// </summary>
+        public void ResetElapsedToZeroAndStartTiming()
+        {
+            elapsedTime = 0;
+            isTriggered = false;
+            IsActivelyTiming = true;
+        }
+        /// <summary>
+        /// Turns off the timer completely.
+        /// </summary>
+        public void ClearStopResetTiming()
+        {
+            elapsedTime = 0;
+            isTriggered = false;
+            multiplier = 1f;
+            IsActivelyTiming = false;
+        }
+
+        /// <summary>
+        /// When used as a stop watch.
+        /// </summary>
+        public bool IsTriggered
+        {
+            get { return isTriggered; }
+        }
+
+        /// <summary>
+        /// When used as a stop watch.
+        /// same as istriggered.
+        /// </summary>
+        public bool IsTimedAmountReached
+        {
+            get { return isTriggered; }
+        }
+
+        /// <summary>
+        /// performs a rotation of time multiplied by 2 pi
+        /// </summary>
+        public float GetElapsedTimeAsRadianRotation
+        {
+            get { return GetElapsedPercentageOfTimer * 2 * pi; }
+        }
+        /// <summary>
+        /// Gets back the time in the form of the Sin portion of Sin Cos
+        /// </summary>
+        public float GetElapsedTimeAsSine
+        {
+            get { return (float)Math.Sin(GetElapsedPercentageOfTimer * 2 * pi); }
+        }
+        /// <summary>
+        /// Gets back the time in the form of the Cos portion of Sin Cos
+        /// </summary>
+        public float GetElapsedTimeAsCosine
+        {
+            get { return (float)Math.Cos(GetElapsedPercentageOfTimer * 2 * pi); }
+        }
+        /// <summary>
+        /// Get the elapsed time as a percentage from o to 1
+        /// </summary>
+        public float GetElapsedPercentageOfTimer
+        {
+            get
+            {
+                if (timer > 0f)
+                    return elapsedTime / timer;
+                else
+                    return 0;
+            }
+        }
+        /// <summary>
+        /// Get the elapsed time as a percentage from 1 to 0
+        /// </summary>
+        public float GetElapsedPercentageOfTimerInverted
+        {
+            get
+            {
+                if (timer > 0f)
+                    return 1f - (elapsedTime / timer);
+                else
+                    return 0;
+            }
+        }
+        /// <summary>
+        /// performs a linear oscilation on the given time this ranges from 0 to 1 to 0
+        /// </summary>
+        public float GetElapsedPercentageAsOscillation
+        {
+            get
+            {
+                if (timer > 0f)
+                {
+                    var half = timer * .5f;
+                    var n = (elapsedTime - half) / half;
+                    if (n < 0f)
+                        n = -n;
+                    return 1f - n;
+                }
+                else
+                    return 0;
+            }
+        }
+        /// <summary>
+        /// performs a linear oscilation on the given time this ranges from 1 to 0 to 1
+        /// </summary>
+        public float GetElapsedPercentageAsOscillationInverted
+        {
+            get
+            {
+                return 1f - GetElapsedPercentageAsOscillation;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if the time reaches its timer amount.
+        /// Calls to the float overload.
+        /// </summary>
+        public bool Update(GameTime gameTime)
+        {
+            return Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+        }
+        /// <summary>
+        /// Returns true if the time reaches its timer amount.
+        /// Timed values increment till they reach the set amount.
+        /// </summary>
+        public bool Update(float elapsedTimeInSeconds)
+        {
+            elapsedTime += elapsedTimeInSeconds * multiplier;
+            if (elapsedTime >= timer)
+            {
+                isTriggered = true;
+                elapsedTime = timer;
+            }
+            return isTriggered;
+        }
+        /// <summary>
+        /// Updates continuously typically used for oscillarions or looping algorithms.
+        /// If the timer increments to its max amount it wraps around to zero and continues.
+        /// Calls to the float overload.
+        /// </summary>
+        public void UpdateContinuously(GameTime gameTime)
+        {
+            UpdateContinuously((float)(gameTime.ElapsedGameTime.TotalSeconds));
+        }
+        /// <summary>
+        /// Updates continuously typically used for oscillarions or looping algorithms.
+        /// If the timer increments to its max amount it wraps around to zero and continues.
+        /// </summary>
+        public void UpdateContinuously(float elapsedTimeInSeconds)
+        {
+            elapsedTime += elapsedTimeInSeconds * multiplier;
+            if (elapsedTime >= timer)
+            {
+                elapsedTime -= timer;
+                IsActivelyTiming = true;
+            }
+        }
+    }
+}
