6f247f9105d8fb79b754c8016ff942bfec21f104
Alendon <Alexander@Familiepanick.de>
2022-09-12 15:56:08 +0200

    FadeableColour::ToHumanReadableString added.

--- FadeableColour.cs
+++ FadeableColour.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Raylib_cs;
 
 namespace Match_3;
@@ -6,7 +7,7 @@ public struct FadeableColour : IEquatable<FadeableColour>
 {
     private Color toWrapp;
     public float CurrentAlpha, TargetALpha, AlphaSpeed, ElapsedTime;
-    
+
     private FadeableColour(Color color)
     {
         toWrapp = color;
@@ -15,17 +16,41 @@ public struct FadeableColour : IEquatable<FadeableColour>
         AlphaSpeed = 0f;
         ElapsedTime = 0f;
     }
-    
+
     private static float Lerp(float? firstFloat, float secondFloat, float? by)
     {
-        return firstFloat ?? (float)(firstFloat * (1 - by) + secondFloat * by);
+        return firstFloat ?? (float) (firstFloat * (1 - by) + secondFloat * by);
     }
+
+    private static readonly Dictionary<Color, string> strings = new ()
+    {
+        {Color.BLACK, "Black"},
+        {Color.BLUE, "Blue"},
+        {Color.BROWN, "Brown"},
+        {Color.DARKGRAY, "DarkGray"},
+        {Color.GOLD, "Gold"},
+        {Color.GRAY, "Gray"},
+        {Color.GREEN, "Green"},
+        {Color.LIGHTGRAY, "LightGray"},
+        {Color.MAGENTA, "Magenta"},
+        {Color.MAROON, "Maroon"},
+        {Color.ORANGE, "Orange"},
+        {Color.PINK, "Pink"},
+        {Color.PURPLE, "Purple"},
+        {Color.RAYWHITE, "RayWhite"},
+        {Color.RED, "Red"},
+        {Color.SKYBLUE, "SkyBlue"},
+        {Color.VIOLET, "Violet"},
+        {Color.WHITE, "White"},
+        {Color.YELLOW, "Yellow"}
+    };
 
     public string ToReadableString()
     {
-        //......
+        Color compare = toWrapp;
+        compare.a = byte.MaxValue;
+        return strings.TryGetValue(compare, out var value) ? value : toWrapp.ToString();
     }
-    
     private Color Lerp(float degree)
     {
         toWrapp = Raylib.ColorAlpha(toWrapp, degree);
@@ -44,7 +69,7 @@ public struct FadeableColour : IEquatable<FadeableColour>
     }
 
     public static bool operator ==(FadeableColour c1, FadeableColour c2) =>
-        c1.toWrapp.a == c2.toWrapp.a && 
+        c1.toWrapp.a == c2.toWrapp.a &&
         c1.toWrapp.b == c2.toWrapp.b &&
         c1.toWrapp.g == c2.toWrapp.g &&
         c1.toWrapp.r == c2.toWrapp.r;
@@ -64,8 +89,8 @@ public struct FadeableColour : IEquatable<FadeableColour>
     {
         return HashCode.Combine(toWrapp, CurrentAlpha);
     }
-    
+
     public static bool operator !=(FadeableColour c1, FadeableColour c2) => !(c1 == c2);
-    
+
     public override string ToString() => nameof(toWrapp);
 }
