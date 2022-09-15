f01f93b3f76584a08724ba584961a27adc008255
H o t i, Shpend <shho310795@proton.me>
2022-09-16 00:53:16 +0200

    Added function SetNextLvl() to init a new level after the player won the previous one.

---
+++ GameState.cs
@@ -0,0 +1,15 @@
+﻿namespace Match_3
+{
+    public record GameState(int GameStartAt, 
+                            int GameOverScreenTime, 
+                            int TilemapWidth,  
+                            int TilemapHeight,
+                            Dictionary<ShapeKind, int> ToCollect,
+                            int TileSize)
+    {
+        public int WINDOW_HEIGHT => TilemapHeight * TileSize;
+        public int WINDOW_WIDTH => TilemapWidth * TileSize;
+
+        public Int2 MIDDLE_TOP => new Int2(WINDOW_WIDTH/2, 0) - Int2.UnitX*25;
+    }
+}
