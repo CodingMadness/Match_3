a6217587850ea19b2701c2f1f30df81adf694e5d
H o t i, Shpend <shho310795@proton.me>
2022-09-16 06:10:02 +0200

    Added a MiDDLE property to the GameState.cs to store always the Center of the screen

--- GameState.cs
+++ GameState.cs
@@ -10,6 +10,7 @@
         public int WINDOW_HEIGHT => TilemapHeight * TileSize;
         public int WINDOW_WIDTH => TilemapWidth * TileSize;
 
-        public Int2 MIDDLE_TOP => new Int2(WINDOW_WIDTH/2, 0) - Int2.UnitX*25;
+        public Int2 TopCenter => new Int2(WINDOW_WIDTH/2, 0) - Int2.UnitX*25;
+        public Int2 Center => new Int2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2) + Int2.UnitX * 20;
     }
 }
