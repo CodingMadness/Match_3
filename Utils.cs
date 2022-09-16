6e3003821a693b33d4eb75c66df418454289a8a6
H o t i, Shpend <shho310795@proton.me>
2022-09-16 06:12:13 +0200

    Added a generic purpose utils class

---
+++ Utils.cs
@@ -0,0 +1,13 @@
+﻿namespace Match_3
+{
+    public static class Utils
+    {
+        public static int RoundValueToNearestOf3(Random rnd, Range r)
+        {
+            const float toRoundTo = 3.0f;
+            int value = rnd.Next(r.Start.Value, r.End.Value);
+            value = (int)(value % toRoundTo == 0 ? value : ((int)MathF.Round(value / toRoundTo)) * toRoundTo);
+            return value;
+        }
+    }
+}
