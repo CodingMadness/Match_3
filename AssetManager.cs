e2c3a053448d8c064e8e135fc4dbcba84a55fa98
H o t i, Shpend <shho310795@proton.me>
2022-09-19 17:42:24 +0200

    Improved the AssetManager.cs by a parameter which determines which exact folder shall be used after the Asset/ folder!

--- AssetManager.cs
+++ AssetManager.cs
@@ -15,23 +15,29 @@ public static class AssetManager
         
     }
     
-    private static string GetAssetfolderName()
+    private static string GetAssetfolderName(string? nextFolder)
     {
         var net6Path = Environment.CurrentDirectory.AsSpan();
         const string projectName = "Match3_Backup";
         int lastProjectNameOccurence = net6Path.LastIndexOf(projectName, StringComparison.Ordinal) + projectName.Length;
-        var projectPath = net6Path.Slice(0, lastProjectNameOccurence);
-        var assetFolderName = Environment.OSVersion.Platform == PlatformID.Unix ? "/Assets/" : "\\Assets\\";
+        var projectPath = net6Path[..lastProjectNameOccurence];
+
+        string assetFolderName;
+
+        if (nextFolder is null)
+            assetFolderName = Environment.OSVersion.Platform == PlatformID.Unix ? $"/Assets/" : $"\\Assets\\";
+
+        else
+            assetFolderName = Environment.OSVersion.Platform == PlatformID.Unix ? $"/Assets/{nextFolder}/" : $"\\Assets\\{nextFolder}\\";
+
         return $"{projectPath}{assetFolderName}"; 
     }
 
+    
     public static void Init()
-    {
-        var assetFolder = GetAssetfolderName();
-        DebugFont = Raylib.LoadFont(assetFolder + "font3.otf");
-        WelcomeFont = Raylib.LoadFont(assetFolder + "font3.otf");
-        //SpriteSheet = Raylib.LoadTexture(assetFolder + "shapes.png");
-        SpriteSheet = Raylib.LoadTexture(assetFolder + "candies.png");
-    }
-     
+    { 
+        DebugFont = Raylib.LoadFont(GetAssetfolderName("fonts") + "font3.otf");
+        WelcomeFont = Raylib.LoadFont(GetAssetfolderName("fonts") + "font3.otf");
+        SpriteSheet = Raylib.LoadTexture(GetAssetfolderName("spritesheets") + "set1.png");
+    }     
 }
