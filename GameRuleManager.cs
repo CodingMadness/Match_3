a9087c54afdebdfb555a0a099aa21a4073fd22c3
Lazarus <shho310795@proton.me>
2022-09-23 23:24:23 +0200

    Splitted the DefineNewLevel() into 2 methods:   1) DefineMatch3Quest(int[] arr)      this checks how many tiles we already have per Balltype and then creates with a given logic a reasonable amount of them but always knowing the limit.   2) DefineNewLevel() ---_> inits only the  internal Level.cs to proper values

--- GameRuleManager.cs
+++ GameRuleManager.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.SymbolStore;
 using Match_3.GameTypes;
 using Raylib_CsLo;
 
@@ -7,19 +8,18 @@ public static class GameRuleManager
 {
     private static int MaxCapacity => (int)Balls.Length * 5;
     public const int Max3PerKind = 3;
-    private static readonly Dictionary<Balls, int> ToCollect = new(MaxCapacity);
     private static readonly Random rnd = new(DateTime.UtcNow.Millisecond);
-
-    public static GameState State { get; private set; }
+    public static int Level { get; private set; }
+    public static Level State { get; private set; }
 
     static GameRuleManager()
     {
-        State = new(30, 3, 15, 10, ToCollect, 64, 3);
+        State = new(30, 3, 6, 5, (-1, new Dictionary<Balls, int>((int)Balls.Length)), 64);
     }
-
+    public static bool ShallMakeRndQuests { get; set; }
     public static void LogQuest(bool useConsole)
     {
-        foreach (var pair in ToCollect)
+        foreach (var pair in State.QuestPerLevel.Quest)
         {
             if (useConsole)
             {
@@ -28,47 +28,64 @@ public static class GameRuleManager
             }
             else
             {
-                string txt = $"You have to collect {pair.Value} {pair.Key}-tiles!";
+                //string txt = $"You have to collect {pair.Value} {pair.Key}-tiles!";
+                string txt = "blabla";
                 GameText logText = new(AssetManager.DebugFont, txt, new(State.Center.X, 3*ITile.Size), 5, Raylib.RED);
-                Program.DrawScaledFont(logText.ScaleText());
+                Program.DrawScaledFont(logText.AlignText());
             }
         }
-    }
-
-    public static void SetCollectQuest()
-    {
-        for (int i = 0; i < (int)Balls.Length-1; i++)
-        {
-            int count = rnd.Next(Max3PerKind - 1, Max3PerKind + 2);
-            State!.ToCollect.TryAdd((Balls)i, count);
-        }
-    }
-
+    } 
     public static bool TryGetSubQuest(CandyShape shape, out int number)
     {
-        return ToCollect.TryGetValue(shape.Ball, out number);
+        return State.QuestPerLevel.Quest.TryGetValue(shape.Ball, out number);
     }
-
-    public static void RemoveSubQuest(CandyShape shape) => ToCollect.Remove(shape.Ball);
-
+    public static void RemoveSubQuest(CandyShape shape) => State.QuestPerLevel.Quest.Remove(shape.Ball);
     public static bool IsSubQuestDone(CandyShape shape, int alreadyMatched) =>
         TryGetSubQuest(shape, out int result) && alreadyMatched >= result;
-
     public static void ChangeSubQuest(CandyShape shape, int toChangeWith)
-        => State.ToCollect[shape.Ball] = toChangeWith;
-       
-    public static bool IsQuestDone() => ToCollect.Count == 0;
-
-    public static void LoadLevel() 
+        => State.QuestPerLevel.Quest[shape.Ball] = toChangeWith;
+    public static bool IsQuestDone() => State.QuestPerLevel.Quest.Count == 0;
+    public static void DefineMatch3Quest(int[] countsPerBall)
     {
-        State.ToCollect.Clear();
-        SetCollectQuest();
-        LogQuest(true);
+        static void SetCollectQuest(int[] countsPerBall)
+        {
+            var level = State.QuestPerLevel.level;
+        
+            int BallCount = 0;
+        
+            if (ShallMakeRndQuests)
+                BallCount = rnd.Next(1, level + 1);
+            else
+            {
+                //THis states that under 4 level we only do increase the Ballcount the player
+                //has to match, so in lvl0: Ball1 --> 2xmatch3,  Ball2 ---> 3xmatch3
+                if (level <= 4)
+                {
+                    for (int i = 0; i <= level; i++)
+                    {
+                        BallCount += 2;
+                    }
+                    for (int i = 0; i < BallCount; i++)
+                    {
+                        int match3Count = rnd.Next(1, 2);
 
-        int startUpTime = Utils.Round(rnd, 5..5, 3);
-        int tileWidth = Utils.Round(rnd, 10..8, 3);
-        int tileHeight = Utils.Round(rnd, 10..9, 3);
-        int gameOverTime = 52;
-        State = new(startUpTime, gameOverTime, tileWidth, tileHeight, ToCollect, ITile.Size, 4);
+                        if (match3Count < countsPerBall[i])
+                            State.QuestPerLevel.Quest.TryAdd((Balls)i, match3Count);
+                        else
+                            State.QuestPerLevel.Quest.TryAdd((Balls)i, countsPerBall[i] - match3Count);
+                    }
+                }
+            }
+        }
+        SetCollectQuest(countsPerBall);
+        LogQuest(true);
+    }
+    public static void DefineNewLevel() 
+    {
+        State.SetNextLevel();
+        State.TilemapWidth += 4;
+        State.TilemapHeight += 2;
+        State.GameStartAt -= 5;
+        const int gameOverTime = 4;
     }
 }
