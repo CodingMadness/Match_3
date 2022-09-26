8ff70da472cfaef6368daebcdd9ac35612981ddb
Lazarus <shho310795@proton.me>
2022-09-26 23:27:41 +0200

    Added logic to load all sorts of assets from embedded-resource-stream into memory for direct access.

--- AssetManager.cs
+++ AssetManager.cs
@@ -1,18 +1,22 @@
-using System.Numerics;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using Raylib_CsLo;
 using static Raylib_CsLo.Raylib;
 
 namespace Match_3;
 
-public static class AssetManager
+public static unsafe class AssetManager
 {
-    public static Texture SpriteSheet { get; private set; }
+    public static Texture ColorBallsTexture { get; private set; }
+    public static Texture MatchBlockAtlas { get; private set; }
+
     public static Font WelcomeFont;
-    
-    public static string GetAssetfolderName(string? nextFolder)
+
+    public static string GetAssetFolderName(string? nextFolder)
     {
         //this line has to be because when calling the .exe from the bin folder, 
-        //rider apperently sets the currentDirectory to /usr/shpend and not as I expect to the
+        //rider apparently sets the currentDirectory to /usr/shpend and not as I expect to the
         //full location of the 
         Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
         var net6Path = Environment.CurrentDirectory.AsSpan();
@@ -30,13 +34,45 @@ public static class AssetManager
         else
             assetFolderName = Environment.OSVersion.Platform == PlatformID.Unix ? $"/Assets/{nextFolder}/" : $"\\Assets\\{nextFolder}\\";
 
-        return $"{projectPath}{assetFolderName}"; 
+        return $"{projectPath}{assetFolderName}";
     }
-    
-    public static void Init(Vector2 initPosOfWelcomeFont)
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="relativePath">the path looks like: Root.Sub or Fonts.font3.oft</param>
+    /// <exception cref="FileNotFoundException"></exception>
+    public static byte[] GetEmbeddedResource(string relativePath)
     {
-        WelcomeFont = GetFontDefault();
-        //WelcomeFont.baseSize = 64;
-        SpriteSheet = LoadTexture(GetAssetfolderName("spritesheets") + "set1.png");
+        var fileName = $"Match_3.Assets.{relativePath}";
+        var assembly = Assembly.GetEntryAssembly();
+        var stream = assembly?.GetManifestResourceStream(fileName);
+        MemoryStream ms = new MemoryStream();
+
+        if (stream == null)
+        {
+            throw new FileNotFoundException("Cannot find mappings file.", nameof(fileName) + ": " + fileName);
+        }
+        stream.CopyTo(ms);
+        var buffer = ms.GetBuffer();
+        return buffer;
+    }
+
+    public static void Init()
+    {
+        var buffer = GetEmbeddedResource("Fonts.font2.otf");
+        byte* first = (byte*)Unsafe.AsPointer(ref buffer[0]);
+        WelcomeFont = LoadFontFromMemory(".otf", first, buffer.Length, 20, null, 0);
+
+        buffer = GetEmbeddedResource("Atlas.set1.png");
+        first = (byte*)Unsafe.AsPointer(ref buffer[0]);
+        Image ballImg = LoadImageFromMemory(".png", first, buffer.Length);
+        ColorBallsTexture = LoadTextureFromImage(ballImg);
+
+
+        buffer = GetEmbeddedResource("Atlas.set2.png");
+        first = (byte*)Unsafe.AsPointer(ref buffer[0]);
+        ballImg = LoadImageFromMemory(".png", first, buffer.Length);
+        MatchBlockAtlas = LoadTextureFromImage(ballImg);
     }
 }
