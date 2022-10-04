0a1ffd4ea0986720d998032a3c6653a6d7f617ab
Lazarus <shho310795@proton.me>
2022-10-04 21:40:43 +0200

    Fixed the bug related to the Box/Bounding box of the respective match not being created as it should, and therefor the "Begin" was always pointing at garbage position.

--- Matches.cs
+++ Matches.cs
@@ -6,83 +6,100 @@ namespace Match_3;
 public class MatchX
 {
     protected readonly ISet<Tile> _matches;
+    private IOrderedEnumerable<Tile>? _orderedSet;
     private Vector2 _first = -Vector2.One;
     private bool _wasRow;
     private Vector2 _placeHere;
-    private readonly int _matchCount;
-
+    public readonly int AllowedMatchCount;
+    private Rectangle _worldRect;
+    
     public bool IsRowBased => _matches.IsRowBased();
     public int Count => _matches.Count;
+
+    public bool IsMatch => Count == AllowedMatchCount;
+    
     public TileShape? Match3Body { get; private set; }
-    public Rectangle MapRect { get; private set; }
+    public Rectangle WorldBox
+    {
+        get
+        {
+            if (!_worldRect.IsEmpty())
+                return _worldRect;
+
+            _orderedSet ??= _matches.Order(CellComparer.Singleton);
+            
+            foreach (var tile in _orderedSet)
+            {
+                _worldRect.Add(tile.WorldBounds);
+            }
+            return _worldRect;
+        }
+    }
     public Vector2 Begin
     {
         get
         {
-            if (MapRect.IsEmpty())
-                return Vector2.Zero;
+            if (_worldRect.IsEmpty())
+                return Utils.INVALID_CELL;
 
-            //var tmp = MapRect with { x = MapRect.width / Count, y = MapRect.width / Count };
-            //the comment above is the actual real code i will use! but its buggy for now!
-            var tmp = _matches.ElementAt(0).MapCell / Tile.Size;
-            return tmp;
+            return _worldRect.GetBegin();
         }
     }
-    public MatchX(int matchCount)
+    public MatchX(int allowedMatchCount)
     {
-        _matchCount = matchCount;
-        _matches = new HashSet<Tile>(matchCount);
+        _worldRect = default;
+        AllowedMatchCount = allowedMatchCount;
+        _matches = new HashSet<Tile>(allowedMatchCount);
     }
 
     /// <summary>
     /// Reorder the Match if it has a structure like: (x0,x1,y2) or similar
     /// </summary>
-    public void AsRow()
-    {
-        
-    }
-    
     public void Add(Tile matchTile, Grid grid)
     {
+        //this entire algorithm only works for a Match3.. rework at some later stag
         void ForceMatchAxisAligned()
         {
             var current = matchTile.GridCell;
-            //Console.WriteLine(matchTile.GridCell);
             _first = _first == -Vector2.One ? current : _first;
-            
-            
-            if (Count == _matchCount)
+
+            if (_first != current && _placeHere == default)
+            {
+                _placeHere = _first.GetOpposite(current);
+            }
+
+            if (Count == AllowedMatchCount) 
             {
                 if (_first.CompletelyDifferent(current) || _first != (current))
                 {
-                    _placeHere = _first.GetOpposite(current);
-                    var tmpTile = grid[current];
-                    grid[_placeHere] = tmpTile;
+                    //DOES NOT WORK YET!......
+                    grid[_placeHere] = grid[current];
+                    grid[current] = Bakery.CreateTile(current, 0.45f);
+                    //grid.Swap(grid[_placeHere], grid[current]);
                 }
             }
         }
-        
+      
         if (_matches.Add(matchTile))
         {
-            //Console.WriteLine("Add() --> " + matchTile.GridCell);
-            ForceMatchAxisAligned();
-            MapRect = MapRect.Add(matchTile.Bounds);
             Match3Body ??= (matchTile.Body as TileShape)!.Clone() as TileShape;
         }
     }
     public void Empty()
     {
+        _worldRect = Utils.INVALID_RECT;
         _wasRow = false;
         _first = -Vector2.One;
         _placeHere = _first;
-        MapRect = default;
         _matches.Clear();
     }
     public EnemyMatches AsEnemies(Grid map)
     {
         EnemyMatches list = new(Count);
 
-        foreach (var match in _matches.Order(CellComparer.Singleton))
+        _orderedSet ??= _matches.Order(CellComparer.Singleton);
+        
+        foreach (var match in _orderedSet)
         {
             map[match.GridCell] = Bakery.AsEnemy(match);
             EnemyTile e = (EnemyTile)map[match.GridCell]!;
@@ -96,14 +113,14 @@ public class MatchX
 public class EnemyMatches : MatchX
 {
     private Rectangle _border;
-    public EnemyMatches(int matchCount) : base(matchCount)
+    public EnemyMatches(int allowedMatchCount) : base(allowedMatchCount)
     {
-        //MapRect = default;
+        //WorldBox = default;
     }
     private Rectangle BuildBorder()
     {
         if (_matches.Count == 0)
-            throw new MethodAccessException($"This method is accessed even tho {MapRect} seems to be empty");
+            throw new MethodAccessException($"This method is accessed even tho {WorldBox} seems to be empty");
 
         Vector2 begin;
         int match3RectWidth;
@@ -115,8 +132,8 @@ public class EnemyMatches : MatchX
             //-----------------|
             // X     Y      Z  |
             //-----------------|
-            var first = _matches.OrderBy(x => x.GridCell.X).ElementAt(0);
-            begin = first.GridCell - Vector2.One;
+            var firstSlot = Begin / Tile.Size;
+            begin = firstSlot - Vector2.One;
             match3RectWidth = _matches.Count + 2;
             match3RectHeight = _matches.Count;
         }
@@ -129,12 +146,12 @@ public class EnemyMatches : MatchX
             // *  Z  *  |
             // *  *  *  |
             //----------|
-            var first = _matches.OrderBy(x => x.GridCell.Y).ElementAt(0);
-            begin = first.GridCell - Vector2.One;
+            var firstSlot = Begin / Tile.Size;
+            begin = firstSlot - Vector2.One;
             match3RectWidth = _matches.Count;
             match3RectHeight = _matches.Count+2;
         }
-        return Utils.GetMatch3Rect(begin, match3RectWidth, match3RectHeight);
+        return Utils.NewWorldRect(begin, match3RectWidth, match3RectHeight);
     }
     public Rectangle Border
     {
