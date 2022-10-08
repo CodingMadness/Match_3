e9b243f43d8cc4c8eb7d6b9805b66adf043cde41
Lazarus <shho310795@proton.me>
2022-10-08 09:41:17 +0200

    Removed the QuestState to QuestHandler.cs

--- GameTypes/QuestHandler.cs
+++ GameTypes/QuestHandler.cs
@@ -1,5 +1,13 @@
 namespace Match_3.GameTypes;
 
+public struct Numbers
+{
+    public (int Count, int CountDown) Click;
+    public (int AllowedSwaps, int CountDown) Swaps;
+    public int Changes;
+    public (int Count, float Time) Match;
+}
+
 public sealed class QuestState
 {
     public bool WasSwapped;
@@ -22,13 +30,13 @@ public abstract class QuestHandler
     }
 
     protected GoalPerType _goalPerType { get; private set; }
-    
+
     //For instance:
     //Collect N-Type1, N-Type2, N-Type3 up to maxTilesActive
     //----->within a TimeSpan of X-sec
     //----->without any miss-swap!
     //the goal is to make per new Level the Quests harder!!
-    
+
     protected QuestHandler()
     {
         _goalPerType = new();
@@ -46,31 +54,31 @@ public abstract class QuestHandler
     }
 }
 
-file class SwapQuestHandler : QuestHandler
-{    
+public class SwapQuestHandler : QuestHandler
+{
     protected override void DefineGoals(QuestState? inventory)
     {
         Numbers eventData = default;
-        
+
         for (Type i = 0; i < Type.Length; i++)
         {
             switch (Game.Level.ID)
             {
                 case 0:
                     eventData.Swaps.AllowedSwaps = Utils.Randomizer.Next(6, 8);
-                    eventData.Swaps.CountDown = (int)(Game.Level.GameBeginAt / 6f); 
+                    eventData.Swaps.CountDown = (int)(Game.Level.GameBeginAt / 6f);
                     break;
                 case 1:
                     eventData.Swaps.AllowedSwaps = Utils.Randomizer.Next(4, 6);
-                    eventData.Swaps.CountDown = (int)(Game.Level.GameBeginAt / 4f); 
+                    eventData.Swaps.CountDown = (int)(Game.Level.GameBeginAt / 4f);
                     break;
                 case 2:
                     eventData.Swaps.AllowedSwaps = Utils.Randomizer.Next(3, 5);
-                    eventData.Swaps.CountDown = (int)(Game.Level.GameBeginAt / 3f); 
+                    eventData.Swaps.CountDown = (int)(Game.Level.GameBeginAt / 3f);
                     break;
                 case 3:
                     eventData.Swaps.AllowedSwaps = Utils.Randomizer.Next(1, 2);
-                    eventData.Swaps.CountDown = (int)(Game.Level.GameBeginAt / 10f); 
+                    eventData.Swaps.CountDown = (int)(Game.Level.GameBeginAt / 10f);
                     break;
             }
 
@@ -82,7 +90,7 @@ file class SwapQuestHandler : QuestHandler
     {
         Game.OnTileSwapped += HandleMatch;
     }
-    
+
     protected override void HandleMatch(QuestState inventory)
     {
         for (Type i = 0; i < Type.Length; i++)
@@ -102,11 +110,11 @@ file class SwapQuestHandler : QuestHandler
     }
 }
 
-file class MatchQuestHandler : QuestHandler
+public class MatchQuestHandler : QuestHandler
 {
     private Numbers _numbers;
-    
-    public MatchQuestHandler()  
+
+    public MatchQuestHandler()
     {
         _numbers.Match = Game.Level.ID switch
         {
@@ -124,19 +132,19 @@ file class MatchQuestHandler : QuestHandler
     {
         if (inventory is null)
             throw new ArgumentException("Inventory has to have values or we cannot build the QUestHandler");
-        
+
         for (Type i = 0; i < Type.Length; i++)
         {
             int matchesNeeded = _numbers.Match.Count;
-            
+
             int matchSum = matchesNeeded * Level.MAX_TILES_PER_MATCH;
             int maxAllowed = inventory.TotalAmountPerType[(int)i];
-            
+
             if (matchSum < maxAllowed)
                 _numbers.Match.Count = matchesNeeded;
             else
-                _numbers.Match.Count = maxAllowed / Level.MAX_TILES_PER_MATCH ;
-            
+                _numbers.Match.Count = maxAllowed / Level.MAX_TILES_PER_MATCH;
+
             _goalPerType.EventData.TryAdd(i, _numbers);
         }
     }
@@ -147,9 +155,9 @@ file class MatchQuestHandler : QuestHandler
         //or about other events
         //Game -------> QuestHandler--->takes "QuestState" does == with _goalPerType and based on the comparison, it decides what to do!
         _goalPerType.EventData.TryGetValue(inventory.MatchTrigger, out var existent);
-        
+
         bool success = inventory.EventData.TryGetValue(inventory.MatchTrigger, out var goal);
- 
+
         if (success && goal.Match == existent.Match)
         {
             inventory.WasGameWonB4Timeout = _goalPerType.EventData.Count == 0;
