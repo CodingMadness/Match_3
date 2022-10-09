aa062e2c0c9cb0e33a344059a2a418b9f4e3c2ec
Lazarus <shho310795@proton.me>
2022-10-09 06:03:39 +0200

    Made TileSize a constant for easier use.

--- GameTypes/Level.cs
+++ GameTypes/Level.cs
@@ -2,11 +2,12 @@
 
 public record Level(int ID, 
     int GameBeginAt, int GameOverScreenCountdown,
-    int GridWidth, int GridHeight, int TileSize)
+    int GridWidth, int GridHeight)
 {
     public const int MAX_TILES_PER_MATCH = 3;
-    public int WindowHeight => GridWidth * TileSize;
-    public int WindowWidth => GridHeight * TileSize;
+    public const int TILE_SIZE = 64 / 2;
+    public int WindowHeight => GridWidth * TILE_SIZE;
+    public int WindowWidth => GridHeight * TILE_SIZE;
 
     private GameTime _gameTime = GameTime.GetTimer(GameBeginAt);
     public ref GameTime GameTimer => ref _gameTime;
