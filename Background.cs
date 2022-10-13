88fbde0b10e780c5802753ee5a5f0123ef3e6874
Lazarus <shho310795@proton.me>
2022-10-13 20:02:17 +0200

    Added Background.cs to the project again! so all can work!

---
+++ Background.cs
@@ -0,0 +1,21 @@
+using Raylib_CsLo;
+
+namespace Match_3.GameTypes;
+
+public class Background
+{
+    public Shape Body { get; }
+    //public Rectangle ScreenRect = new(0f, 0f, Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+    public Texture Texture;
+    public Background(Texture bgTexture)
+    {
+        Body = new()
+        {
+            AtlasLocation = new(0f, 0f),
+            Size = new(bgTexture.width, bgTexture.height),
+            Form = ShapeKind.Rectangle,
+            Scale = 1f
+        };
+        Texture = bgTexture;
+    }
+}
