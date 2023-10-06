bfe03758fba21ce25c95727f0bb75c547d313ac5
Shpendicus <shho310795@proton.me>
2023-10-06 22:16:40 +0200

    Adapted the file to work with the newest raylib-cs 4.5.04 version

--- AssetManager.cs
+++ AssetManager.cs
@@ -1,19 +1,19 @@
 using System.Reflection;
 using Match_3.GameTypes;
-using Raylib_CsLo;
+using Raylib_cs;
 
 namespace Match_3;
 
 public static unsafe class AssetManager
 {
-    public static Texture WelcomeTexture;
-    public static Texture GameOverTexture;
-    public static Texture DefaultTileAtlas;
-    public static Texture EnemySprite;
-    public static Texture BgIngameTexture;
+    public static Texture2D WelcomeTexture;
+    public static Texture2D GameOverTexture;
+    public static Texture2D DefaultTileAtlas;
+    public static Texture2D EnemySprite;
+    public static Texture2D BgIngameTexture;
     public static Shader WobbleEffect;
     public static (int size, int time) ShaderData;
-    public static Texture FeatureBtn;
+    public static Texture2D FeatureBtn;
     public static Sound SplashSound;
     private static Font GameFont;
     public static GameText WelcomeText = new(GameFont, "Welcome young man!!", 7f);
@@ -26,7 +26,7 @@ public static unsafe class AssetManager
     /// </summary>
     /// <param name="relativePath">the path looks like: Root.Sub or Fonts.font3.oft</param>
     /// <exception cref="FileNotFoundException"></exception>
-    public static byte[] GetEmbeddedResource(string relativePath)
+    private static byte[] GetEmbeddedResource(string relativePath)
     {
         var fileName = $"Match_3.Assets.{relativePath}";
         var assembly = Assembly.GetEntryAssembly();
@@ -42,19 +42,19 @@ public static unsafe class AssetManager
         return buffer;
     }
 
-    private static Sound LoadSound(string fileNameOnly)
+    private static Sound LoadSound(string relativePath)
     {
-        var buffer = GetEmbeddedResource($"Sounds.{fileNameOnly}");
+        var buffer = GetEmbeddedResource($"Sounds.{relativePath}");
         var first = (byte*)Unsafe.AsPointer(ref buffer[0]);
-        var wave = LoadWaveFromMemory(".mp3", first, buffer.Length);
+        sbyte* x = (sbyte*)Unsafe.AsPointer(ref Unsafe.AsRef(".mp3"[0]));
+        var wave = LoadWaveFromMemory(x, first, buffer.Length);
         return LoadSoundFromWave(wave);
     }
 
-    private static Font LoadFont(string fileNameOnly)
+    private static Font LoadFont(string relativePath)
     {
-        var buffer = GetEmbeddedResource($"Fonts.{fileNameOnly}");
-        var first = (byte*)Unsafe.AsPointer(ref buffer[0]);
-        return LoadFontFromMemory(".ttf", first, buffer.Length, 200, null, 0);
+        var buffer = GetEmbeddedResource($"Fonts.{relativePath}");
+        return LoadFontFromMemory(".otf", buffer, 200, null, 0);
     }
 
     /// <summary>
@@ -62,23 +62,16 @@ public static unsafe class AssetManager
     /// </summary>
     /// <param name="fullPath">For instance: Background.bg1.png OR Button.FeatureBtn.png</param>
     /// <returns></returns>
-    private static Texture LoadTexture(string fullPath)
+    private static Texture2D LoadTexture(string fullPath)
     {
         var buffer = GetEmbeddedResource(fullPath);
-        var first = (byte*)Unsafe.AsPointer(ref buffer[0]); 
-        Image bg = LoadImageFromMemory(".png", first, buffer.Length);
+        Image bg = LoadImageFromMemory(".png", buffer);
         return LoadTextureFromImage(bg);
     }
 
-    private static Texture LoadGuiTexture(string relativePath)
-    {
-        return LoadTexture($"Sprites.GUI.{relativePath}");
-    }
-    
-    private static Texture LoadInGameTexture(string fileNameOnly)
-    {
-          return LoadTexture($"Sprites.Tiles.{fileNameOnly}");
-    }
+    private static Texture2D LoadGuiTexture(string relativePath) => LoadTexture($"Sprites.GUI.{relativePath}");
+
+    private static Texture2D LoadInGameTexture(string relativePath) => LoadTexture($"Sprites.Tiles.{relativePath}");
 
     public static (int sizeLoc, int timeLoc) InitShader()
     {
@@ -99,19 +92,19 @@ public static unsafe class AssetManager
         float speedX = 8.0f;
         float speedY = 8.0f;
 
-        SetShaderValue(WobbleEffect, freqXLoc, ref freqX, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
-        SetShaderValue(WobbleEffect, freqYLoc, ref freqY, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
-        SetShaderValue(WobbleEffect, ampXLoc, ref ampX, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
-        SetShaderValue(WobbleEffect, ampYLoc, ref ampY, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
-        SetShaderValue(WobbleEffect, speedXLoc, ref speedX, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
-        SetShaderValue(WobbleEffect, speedYLoc, ref speedY, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
+        SetShaderValue(WobbleEffect, freqXLoc, freqX, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
+        SetShaderValue(WobbleEffect, freqYLoc, freqY, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
+        SetShaderValue(WobbleEffect, ampXLoc, ampX, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
+        SetShaderValue(WobbleEffect, ampYLoc, ampY, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
+        SetShaderValue(WobbleEffect, speedXLoc, speedX, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
+        SetShaderValue(WobbleEffect, speedYLoc, speedY, ShaderUniformDataType.SHADER_UNIFORM_FLOAT);
 
         return (sizeLoc, secondsLoc);
     }
     
-    private static Shader LoadShader(string fileNameOnly)
+    private static Shader LoadShader(string relativePath)
     {
-        var buffer = GetEmbeddedResource($"Shaders.{fileNameOnly}");
+        var buffer = GetEmbeddedResource($"Shaders.{relativePath}");
         using Stream rsStream = new MemoryStream(buffer, 0, buffer.Length);
         using var reader = new StreamReader(rsStream); 
         return LoadShaderFromMemory(null,  reader.ReadToEnd());
@@ -122,7 +115,7 @@ public static unsafe class AssetManager
         InitAudioDevice();
 
         SplashSound = LoadSound("splash.mp3");
-        GameFont = LoadFont("font4.otf");
+        GameFont = LoadFont("font1.otf");
         WelcomeTexture = LoadGuiTexture("Background.bgWelcome1.png");
         FeatureBtn = LoadGuiTexture("Button.btn1.png");
         BgIngameTexture = LoadGuiTexture("Background.bgIngame1.png");
@@ -141,7 +134,7 @@ public static unsafe class AssetManager
     public static void InitGameOverTxt()
     {
         GameOverText.InitSize *= 1f;
-        GameOverText.ScaleText(null);
+        GameOverText.ScaleText(Utils.GetScreenCoord().X);
         GameOverText.Color = RED;
         GameOverText.Begin = (Utils.GetScreenCoord() * 0.5f) with { X = 0f };
     }
@@ -149,7 +142,7 @@ public static unsafe class AssetManager
     public static void InitWelcomeTxt()
     {
         WelcomeText.Color = RED;
-        WelcomeText.ScaleText(null);
+        WelcomeText.ScaleText(Utils.GetScreenCoord().X);
         WelcomeText.Begin = (Utils.GetScreenCoord() * 0.5f) with { X = 0f };
     }
 }
