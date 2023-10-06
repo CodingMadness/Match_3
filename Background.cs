0ef1a82e984f63df1287757e0340f7f2850e5e79
Shpendicus <shho310795@proton.me>
2023-10-06 22:17:47 +0200

    Updated codestyle to C#12 primary ctor()

--- Background.cs
+++ Background.cs
@@ -1,22 +1,17 @@
-using Raylib_CsLo;
+using Raylib_cs;
 
 namespace Match_3;
 
-public class Background
+public class Background(Texture2D bgTexture)
 {
-    public Shape Body { get; }
-    //public Rectangle ScreenRect = new(0f, 0f, Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
-    public Texture Texture;
-    
-    public Background(Texture bgTexture)
+    public Shape Body { get; } = new()
     {
-        Body = new()
-        {
-            AtlasLocation = new(0f, 0f),
-            Size = new(bgTexture.width, bgTexture.height),
-            Form = ShapeKind.Rectangle,
-            Scale = 1f
-        };
-        Texture = bgTexture;
-    }
+        AtlasLocation = new(0f, 0f),
+        Size = new(bgTexture.width, bgTexture.height),
+        Form = ShapeKind.Rectangle,
+        Scale = 1f
+    };
+
+    //public Rectangle ScreenRect = new(0f, 0f, Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+    public Texture2D Texture = bgTexture;
 }
