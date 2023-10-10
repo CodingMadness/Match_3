cea00b900488a382f3e77f8a8502499f89b1a382
Shpendicus <shho310795@proton.me>
2023-10-10 23:14:23 +0200

    Added logic, to buffer the ongoing questlogs from the "DrawLog()" function, I use a stringarray to pool X different logs which is still a bad approach.

--- Workflow/QuestBuilder.cs
+++ Workflow/QuestBuilder.cs
@@ -1,5 +1,7 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
 using System.Text;
+using CommunityToolkit.HighPerformance.Buffers;
 using Match_3.Service;
 using Match_3.Variables;
 
@@ -7,26 +9,31 @@ namespace Match_3.Workflow;
 
 public static class QuestBuilder
 {
-    private const string QuestLog = $"(Black) You have to collect an amount of" +
-                                    $" (EMPTY) <A> EMPTY Matches" +
-                                    $" (Black) and u have in between those," +
-                                    $" (EMPTY) only <B> seconds left" +
-                                    $" (Black) and also just" +
-                                    $" (EMPTY) <C> swaps available" +
-                                    $" (Black) for each new match" +
-                                    $" (Black) and furthermore, you only are allowed to replace any given tile" +
-                                    $" (EMPTY) <D> times at max" +
-                                    $" (Black) for your own help ";
+    private const string TemplateQuestLog = $"(Black) You have to collect an amount of" +
+                                            $" (EMPTY) <A> EMPTY Matches" +
+                                            $" (Black) and u have in between those," +
+                                            $" (EMPTY) only <B> seconds left" +
+                                            $" (Black) and also just" +
+                                            $" (EMPTY) <C> swaps available" +
+                                            $" (Black) for each new match" +
+                                            $" (Black) and furthermore, you only are allowed to replace any given tile" +
+                                            $" (EMPTY) <D> times at max" +
+                                            $" (Black) for your own help ";
 
-    //the length of any string which is inside the GameState class
-    private static readonly StringBuilder Logger = new(QuestLog.Length);
+    // //the length of any string which is inside the GameState class
+
+    private static readonly int ExtraSpace = TemplateQuestLog.Length + TemplateQuestLog.Length / 5;
+    private static readonly StringBuilder LogBuilder = new(ExtraSpace);
+    
+    private static int _questCounter, _repeater;
+    private static readonly string[] QuestLogPool = new string[MatchQuestHandler.Instance.QuestCount];
+    private static bool _shallRecycleLogs;
 
     static QuestBuilder()
     {
-        GameState.Logger = Logger;
     }
-    
-    public static void BuildQuestMsgFrom(in Quest quest)
+
+    private static string BuildQuestMsgFrom(in Quest quest)
     {
         double GetNextValue(in Quest data, int offset)
         {
@@ -58,11 +65,12 @@ public static class QuestBuilder
 
             return numericValue;
         }
-
+         
+        
         string colorAsTxt = $"{quest.ItemType}";
-        Logger.Clear().Append(QuestLog).Replace("EMPTY", colorAsTxt);
+        LogBuilder.Clear().Append(TemplateQuestLog).Replace("EMPTY", colorAsTxt);
 
-        var chunkIterator = new TextStyleEnumerator(Logger);
+        var chunkIterator = new TextStyleEnumerator(LogBuilder);
         int counter = 0;
         char begin = 'A';
 
@@ -75,10 +83,33 @@ public static class QuestBuilder
 
             var value = GetNextValue(quest, counter++);
 
-            Logger.Replace($"<{begin++}>", $"<{value}>");
+            LogBuilder.Replace($"<{begin++}>", $"<{value}>");
         }
+
+        QuestLogPool[_questCounter++] = LogBuilder.ToString();
+        _repeater = _questCounter - 1;
+        _shallRecycleLogs = _questCounter == QuestLogPool.Length;
+        _repeater = _shallRecycleLogs ? 0 : _repeater;
+
+        return QuestLogPool[_repeater];
     }
 
-    public static void Reset() => Logger.Clear();
+    public static string GetQuestLogFromPool(in Quest quest)
+    {
+        string result;
 
+        if (_shallRecycleLogs && _repeater < QuestLogPool.Length)
+            result = QuestLogPool[_repeater++];
+        else if (_repeater == QuestLogPool.Length)
+        {
+            _repeater = 0;
+            result = QuestLogPool[_repeater++];
+        }
+        else
+            result = BuildQuestMsgFrom(quest);
+
+        return result;
+    }
+
+    public static void Reset() => LogBuilder.Clear();
 }
