a21bb1d3d552785b6d81c66da14dee997c640e1d
Shpendicus <shho310795@proton.me>
2023-10-11 03:31:57 +0200

    Added a new Span-Field to "TextInfo", which stores the exact MemberName of a SubQuest.cs Member, in order for that to later replace with the respective value.
    Furthermore I added the corresponding logic in "MoveNext()" to find the MemberName and save him in the textInfo field.
    
    Renamed also "TextStyleEnumerator" to "PhraseEnumerator" because its more fitting.

--- Service/Enumerators.cs
+++ Service/Enumerators.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using DotNext.Buffers;
 using ImGuiNET;
+using Match_3.Variables;
 using NoAlloq;
 
 namespace Match_3.Service;
@@ -19,7 +20,7 @@ public ref struct FastSpanEnumerator<TItem>
 {
     private ref TItem _currentItem;
     private readonly ref TItem _lastItemOffsetByOne;
-    
+
     // public FastSpanEnumerator(SpanEnumerable<TItem> enumerator)
     // {
     //     _currentItem
@@ -39,7 +40,7 @@ public ref struct FastSpanEnumerator<TItem>
         _lastItemOffsetByOne = ref Unsafe.Add(ref _currentItem, length + 1);
     }
 
-    [UnscopedRef]public ref TItem Current => ref _currentItem;
+    [UnscopedRef] public ref TItem Current => ref _currentItem;
 
 
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
@@ -59,7 +60,7 @@ public ref struct FastSpanEnumerator<TItem>
         _currentItem = ref Unsafe.Subtract(ref _currentItem, 1);
         return Unsafe.IsAddressLessThan(ref _currentItem, ref _lastItemOffsetByOne);
     }
-    
+
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     [UnscopedRef]
     public ref FastSpanEnumerator<TItem> GetEnumerator()
@@ -70,28 +71,31 @@ public ref struct FastSpanEnumerator<TItem>
 
 public readonly ref struct TextInfo
 {
-    public readonly ReadOnlySpan<char> Text2Color;
+    public readonly ReadOnlySpan<char> Variable2Replace;
+    public readonly ReadOnlySpan<char> Slice2Color;
     public readonly Vector2 TextSize;
-    public readonly Vector4 ColorV4;
+    public readonly Vector4 ColorV4ToApply;
     public readonly Color SystemColor;
     private readonly char _separator;
 
     /// <summary>
     /// represents the color we want to convert to a Vector4 type
     /// </summary>
-    /// <param name="text2Color">the slice of a span like: (abc def ghi)</param>
+    /// <param name="slice2Color">the slice of a span like: (abc def ghi)</param>
     /// <param name="colorCode">the string colorCode like {Black} or {Red}</param>
+    /// <param name="variable2Replace"></param>
     /// <param name="separator">the char which is being used to iterate over each word within the piece</param>
-    public TextInfo(ReadOnlySpan<char> text2Color, ReadOnlySpan<char> colorCode, char separator = ' ')
+    public TextInfo(ReadOnlySpan<char> slice2Color, ReadOnlySpan<char> colorCode,
+        ReadOnlySpan<char> variable2Replace, char separator = ' ')
     {
         ReadOnlySpan<char> code;
- 
+
         if (colorCode == ReadOnlySpan<char>.Empty)
         {
             code = "Black";
         }
 
-        else if (colorCode.IndexOf('(') == -1)
+        else if (!colorCode.Contains('('))
         {
             code = colorCode;
         }
@@ -100,25 +104,26 @@ public readonly ref struct TextInfo
             code = colorCode[1..^1];
         }
 
-        Text2Color = text2Color;
+        Slice2Color = slice2Color;
         _separator = separator;
         //FastEnum.Parse<KnownColor, int>(code); for some reason this does not work.....
-        var color = Enum.Parse<KnownColor>(code); 
+        var color = Enum.Parse<KnownColor>(code);
         SystemColor = Color.FromKnownColor(color);
-        ColorV4 = SystemColor.ToVec4();
+        ColorV4ToApply = SystemColor.ToVec4();
         Vector2 offset = Vector2.One * 1.5f;
-        TextSize = ImGui.CalcTextSize(text2Color) + offset;
+        TextSize = ImGui.CalcTextSize(slice2Color) + offset;
+        Variable2Replace = variable2Replace;  
     }
 
-    public TextInfo(ReadOnlySpan<char> current, Color sysCol) :
-        this(current, sysCol.Name)
+    public TextInfo(ReadOnlySpan<char> current, Color sysCol, ReadOnlySpan<char> valuePlaceHolder) :
+        this(current, sysCol.Name, valuePlaceHolder)
     {
     }
 
     [UnscopedRef]
     public WordEnumerator GetEnumerator() => new(this, _separator);
 
-    public override string ToString() => Text2Color.ToString();
+    public override string ToString() => Slice2Color.ToString();
 }
 
 public ref struct WordEnumerator
@@ -147,7 +152,7 @@ public ref struct WordEnumerator
                 "it cannot slice the ROS which shall be viewed as string[]");
     }
 
-    public WordEnumerator(in TextInfo original, char separator) : this(original.Text2Color, separator)
+    public WordEnumerator(in TextInfo original, char separator) : this(original.Slice2Color, separator)
     {
         _original = original;
     }
@@ -157,15 +162,15 @@ public ref struct WordEnumerator
         //ReadOnlySpan<char> items = "abc <separator> def <separator> ghi <separator> jkl <separator> mno"
         int idxOfChar = _remainder.IndexOf(_separator);
         ReadOnlySpan<char> word;
-        
+
         //this separate check serves 2 purposes:
-          //1. when "idxOfChar" is -1 and "separator" as well as "_remainder" are empty than indeed
-          //its safe to assume that the entire thing is empty or was built up badly...
+        //1. when "idxOfChar" is -1 and "separator" as well as "_remainder" are empty than indeed
+        //its safe to assume that the entire thing is empty or was built up badly...
         if (idxOfChar == -1)
         {
             if (_separator is (char)32 && _remainder.Length == 0)
                 return false;
-            
+
             //the 2. purpose is to determine, when the above if condition is false, that there is really
             //only 1 word left and we have to treat this separately...
             word = _remainder;
@@ -177,7 +182,7 @@ public ref struct WordEnumerator
             _remainder = _remainder[(word.Length + 1)..];
         }
 
-        _tmp = new(word, _original.SystemColor);
+        _tmp = new(word, _original.SystemColor, "dataType");
 
         return word.Length > 0;
     }
@@ -188,7 +193,7 @@ public ref struct WordEnumerator
     }
 }
 
-public ref struct TextStyleEnumerator
+public ref struct PhraseEnumerator
 {
     private static readonly Regex Rgx = new(@"\([a-zA-Z]+\)", RegexOptions.Singleline);
     private TextInfo _current;
@@ -198,7 +203,7 @@ public ref struct TextStyleEnumerator
     private MemoryRental<(int idx, int len)> _matchPool;
     private int _position;
 
-    public TextStyleEnumerator(ReadOnlySpan<char> text)
+    public PhraseEnumerator(ReadOnlySpan<char> text)
     {
         //dont know a value yet for this but we use 50 for now
         _matchPool = new(50);
@@ -207,7 +212,7 @@ public ref struct TextStyleEnumerator
         //var result = rgx.Split(_text);
         //{Black} This is a {Red} super nice {Green} shiny looking text
         colorPositions = _matchPool.Span;
-        
+
         foreach (var enumerateMatch in Rgx.EnumerateMatches(text))
         {
             //reset:
@@ -221,18 +226,18 @@ public ref struct TextStyleEnumerator
         _position = 0;
     }
 
-    public TextStyleEnumerator(StringBuilder text)
+    public PhraseEnumerator(StringBuilder text)
     {
         //dont know a value yet for this but we use 50 for now
         _matchPool = new(50);
         _position = 0;
         colorPositions = _matchPool.Span;
         //{Black} This is a {Red} super nice {Green} shiny looking text
-        
+
         foreach (var chunk in text.GetChunks())
         {
             _text = chunk.Span;
-            
+
             foreach (var enumerateMatch in Rgx.EnumerateMatches(_text))
             {
                 //reset:
@@ -246,7 +251,7 @@ public ref struct TextStyleEnumerator
         colorPositions = colorPositions.Slice(0, _position);
         _position = 0;
     }
-    
+
     [UnscopedRef] public ref readonly TextInfo Current => ref _current;
 
     public bool MoveNext()
@@ -269,17 +274,21 @@ public ref struct TextStyleEnumerator
             _relativeStart = 1;
         }
 
-        //part0: (Black) You have to collect (Red) xxx
-        var slice2Colorize = _text.Slice(okayBegin, _relativeEnd - _relativeStart);
-        slice2Colorize = slice2Colorize[match.len..^1];
-        _current = new(slice2Colorize, color2Use);
+        var slice2Colorize = _text.Slice(okayBegin, _relativeEnd - _relativeStart)[(match.len + 1)..^1];
+        bool isAMemberName = slice2Colorize.Contains('.'); //like: Match.Count and so on...
+
+        var variable2Replace = isAMemberName
+                                                  ? slice2Colorize[..slice2Colorize.IndexOf(' ')]
+                                                  : ReadOnlySpan<char>.Empty;
+
+        _current = new(slice2Colorize, color2Use, variable2Replace);
         _position++;
 
         return slice2Colorize.Length > 0;
     }
 
     [UnscopedRef]
-    public ref readonly TextStyleEnumerator GetEnumerator()
+    public ref readonly PhraseEnumerator GetEnumerator()
     {
         return ref this;
     }
