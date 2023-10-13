286a89eefa16eb80f492fe82b3a5736bb93bb300
Shpendicus <shho310795@proton.me>
2023-10-13 05:40:29 +0200

    Added 1 more 'SubQuest' to add in the 'QuestLog' const-string to make it harder for the player and also added a 'GetValueByMemberName()' method to give me back in a KVP-fashion, the proper value behind the Const-MemberNames of each SubQuest member

--- Variables/Quest.cs
+++ Variables/Quest.cs
@@ -1,6 +1,14 @@
-﻿namespace Match_3.Variables;
+﻿using DotNext;
+using DotNext.Runtime;
+using FastEnumUtility;
 
-public readonly record struct Quest(TileType TileColor, SubQuest? Match, SubQuest? Swap, SubQuest? Replacement)
+namespace Match_3.Variables;
+
+public readonly record struct Quest(TileColor TileColor, 
+                                    SubQuest? Match, 
+                                    SubQuest? Swap,
+                                    SubQuest? Replacement,
+                                    SubQuest? MissMatch)
 {
     public int CompareReplacements(in AllStats? stats)
     {
@@ -37,4 +45,24 @@ public readonly record struct Quest(TileType TileColor, SubQuest? Match, SubQues
     public const string MatchIntervalName = nameof(Quest.Match)+ "." + nameof(Quest.Match.Value.Interval);
     public const string SwapCountName = nameof(Quest.Swap)+ "." + nameof(Quest.Swap.Value.Count);
     public const string ReplacementCountName = nameof(Quest.Replacement)+ "." + nameof(Quest.Replacement.Value.Count);
+    public const string MissMatchName = nameof(Quest.MissMatch)+ "." + nameof(Quest.MissMatch.Value.Count);
+    
+    public int GetValueByMemberName(ReadOnlySpan<char> name)
+    {
+        //FOR NOW we compare the contents but only until I have written a replace method to work merely on 
+        // Spans and does not create a new copy!!!
+        if (name.BitwiseCompare(MatchCountName) == 0)
+            return Match!.Value.Count;
+        else if (name == MatchIntervalName)
+            return (int)Match!.Value.Interval;
+        else if (name == SwapCountName)
+            return Swap!.Value.Count;
+        else if (name == ReplacementCountName)
+            return Replacement!.Value.Count;
+        else if (name == MissMatchName)
+            return MissMatch!.Value.Count;
+
+        throw new ArgumentException("this code should not be reached at all!");
+    }
+    
 }
