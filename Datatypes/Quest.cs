c6b66cb29a1473b4709a94989a8c74d042e1ae06
CodingMadness <shho310795@proton.me>
2023-10-20 05:59:36 +0200

    Removed the entire Comparisons from "Quest.cs" I do this now only in the QuestHandler.cs classes.

--- Datatypes/Quest.cs
+++ Datatypes/Quest.cs
@@ -1,8 +1,6 @@
 ﻿using DotNext;
-using DotNext.Runtime;
-using FastEnumUtility;
 
-namespace Match_3.Variables;
+namespace Match_3.Datatypes;
 
 public readonly record struct Quest(TileColor TileColor, 
                                     SubQuest? Match, 
@@ -10,36 +8,36 @@ public readonly record struct Quest(TileColor TileColor,
                                     SubQuest? Replacement,
                                     SubQuest? MissMatch)
 {
-    public int CompareReplacements(in AllStats? stats)
-    {
-        return -1;
-    }
-    
-    public int CompareSwaps(in AllStats? stats)
-    {
-        return stats switch
-        {
-            { Swapped: { } statsClick } when Swap is { Count : var count } => count.CompareTo(statsClick.Count),
-            null => 1,
-            { Swapped: null } => 1,
-            { Swapped: { } } when Swap is null => -1,
-            _ => throw new ArgumentOutOfRangeException(nameof(stats), stats, null)
-        };
-    }
-
-    public int CompareMatches(in AllStats? stats)
-    {
-        return stats switch
-        {
-            /*stats.Matched*/
-            { Matched: var matchedStats } when Match is not null
-                => Match.Value.CompareTo(matchedStats!.Value),
-            null => 1,
-            { Matched: null } => 1,
-            { Matched: not null } when Match is null => -1,
-            _ => throw new ArgumentOutOfRangeException(nameof(stats), stats, null)
-        };
-    }
+    // public int CompareReplacements(in Stats? stats)
+    // {
+    //     return -1;
+    // }
+    //
+    // public int CompareSwaps(in Stats? stats)
+    // {
+    //     return stats switch
+    //     {
+    //         { Swapped: { } statsClick } when Swap is { Count : var count } => count.CompareTo(statsClick.Count),
+    //         null => 1,
+    //         { Swapped: null } => 1,
+    //         { Swapped: { } } when Swap is null => -1,
+    //         _ => throw new ArgumentOutOfRangeException(nameof(stats), stats, null)
+    //     };
+    // }
+    //
+    // public int CompareMatches(in Stats? stats)
+    // {
+    //     return stats switch
+    //     {
+    //         /*stats.Matched*/
+    //         { Matched: var matchedStats } when Match is not null
+    //             => Match.Value.CompareTo(matchedStats!.Value),
+    //         null => 1,
+    //         { Matched: null } => 1,
+    //         { Matched: not null } when Match is null => -1,
+    //         _ => throw new ArgumentOutOfRangeException(nameof(stats), stats, null)
+    //     };
+    // }
     
     public const string MatchCountName    = nameof(Quest.Match)+ "." + nameof(Quest.Match.Value.Count);
     public const string MatchIntervalName = nameof(Quest.Match)+ "." + nameof(Quest.Match.Value.Interval);
