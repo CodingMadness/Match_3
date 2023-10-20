49c389e4b5f325926ed57a575cfc08c5357e0e73
CodingMadness <shho310795@proton.me>
2023-10-20 05:48:38 +0200

    Rewrote the entire struct to be a class which contains just contains all nessecary data which is mutable and can be updated when needed.

--- Datatypes/EventStats.cs
+++ Datatypes/EventStats.cs
@@ -1,37 +1,11 @@
-﻿namespace Match_3.Variables;
+﻿namespace Match_3.Datatypes;
 
-public struct EventStats : IComparable<EventStats>
+public class EventStats
 {
-    private TimeOnly? _prev, _current;
-    private int _count;
-
-    public EventStats(int count) : this()
-    {
-        Count = count;
-    }
-
-    public float Interval { get; private set; }
-
-    public int Count
-    {
-        get => _count;
-        set
-        {
-            _count = value;
-            _current = TimeOnly.FromDateTime(DateTime.Now);
-            _prev = _prev is null ? _current : TimeOnly.FromTimeSpan((_current - _prev).Value);
-            Interval = _prev?.Second ?? 0f;
-        }
-    }
-
-    public override string ToString()
-    {
-        return $"event: ({Count} was done in an Interval of: {Interval} seconds) {Environment.NewLine}";
-    }
-
-    public int CompareTo(EventStats other)
-    {
-        var countComparison = _count.CompareTo(other._count);
-        return countComparison == 0 ? Interval.CompareTo(other.Interval) : countComparison;
-    }
+    public float Interval;
+    public int Count;
+    public Tile? TileX;
+    public Tile? TileY;
+    public MatchX? Matches;
+    public bool wasMatch, StillTimeLeft, WasSwapped;
 }
