7da55335d9a256a0fcac5bb4050c798c77ab9b22
CodingMadness <shho310795@proton.me>
2023-10-20 06:08:02 +0200

    Removed the obsolete "RuleHandler" stuff from the SingletonManager.
    Removed the "SwapQuestHandler" and I now only do the entire Quest-handling and whatever comparison in the "MatchQuestHandler"
    
    Also removed every final-comparison when game is ending and will do a separate class for this later on...

--- Workflow/QuestHandler.cs
+++ Workflow/QuestHandler.cs
@@ -1,17 +1,18 @@
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using Match_3.Variables;
+using Match_3.Datatypes;
 
 namespace Match_3.Workflow;
 
 public static class SingletonManager
 {
     private const byte MaxQuestHandlerInstances = 5;
-    private const byte MaxRuleHandlerInstances = 1;
 
     public static readonly Dictionary<Type, QuestHandler> QuestHandlerStorage = new(MaxQuestHandlerInstances);
-    public static readonly Dictionary<Type, RuleHandler> RuleHandlerStorage = new(MaxRuleHandlerInstances);
 
-    public static T GetOrCreateQuestHandler<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.NonPublicConstructors)]T>() where T : QuestHandler
+    public static T GetOrCreateQuestHandler<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.NonPublicConstructors)]
+        T>() where T : QuestHandler
     {
         lock (QuestHandlerStorage)
         {
@@ -23,36 +24,18 @@ public static class SingletonManager
 
             toReturn = Activator.CreateInstance(typeof(T), true) as QuestHandler;
 
-            QuestHandlerStorage.Add(typeof(T), toReturn!);
-
-            return (T)toReturn!;
-        }
-    }
-
-    public static T GetOrCreateRuleHandler<T>() where T : RuleHandler
-    {
-        lock (QuestHandlerStorage)
-        {
-            if (RuleHandlerStorage.TryGetValue(typeof(T), out var toReturn))
-            {
-                return (T)toReturn;
-                //got val to return
-            }
-
-            toReturn = (RuleHandler?)Activator.CreateInstance(typeof(T), true);
-
-            RuleHandlerStorage.Add(typeof(T), toReturn!);
+            QuestHandlerStorage.Add(typeof(T), toReturn);
 
             return (T)toReturn;
         }
     }
 }
 
-
 /// <summary>
 ///The Game notifies the QuestHandler, when a matchX happened or a tile was swapped
 ///or about other events
-///Game -------->(notifies) QuestHandler-------->(compares) "GameState" with "Goal" and based on the comparison, it decides what to do!
+///Game -------->(notifies) QuestHandler-------->(compares) "GameState" with "Quest"
+/// and based on the comparison, it decides what to do!
 /// For instance
 /// Collect N-Type1, N-Type2, N-Type3 up to maxTilesActive
 ///----->within a TimeSpan of X-sec
@@ -62,18 +45,21 @@ public static class SingletonManager
 public abstract class QuestHandler
 {
     private readonly Type _self;
-    
+
     protected int QuestCounter { get; set; }
 
-    protected int QuestLeft => QuestBuilder.QuestCount - QuestCounter;
-    
-    protected bool IsMainGoalReached => QuestLeft == 0;
-    
+    protected int QuestsLeft => QuestBuilder.QuestCount - QuestCounter;
+
+    protected bool IsMainGoalReached => QuestsLeft == 0;
+
     private bool IsActive { get; set; }
 
-    protected static THandler GetInstance<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.NonPublicConstructors)]THandler>() where THandler : QuestHandler
+    protected static THandler GetInstance<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.NonPublicConstructors)]
+        THandler>()
+        where THandler : QuestHandler
         => SingletonManager.GetOrCreateQuestHandler<THandler>();
-    
+
     /// <summary>
     ///The Game notifies the QuestHandler, when a matchX happened or a tile was swapped
     ///or about other events
@@ -89,25 +75,25 @@ public abstract class QuestHandler
         _self = self;
     }
 
-    protected static bool IsSubQuestReached(EventType eventType, in Quest quest, in AllStats allStats,
-        out int direction)
-    {
-        direction = default;
+    // protected static bool IsSubQuestReached(EventType eventType, in Quest quest,
+    //     in Stats stats,
+    //     out int direction)
+    // {
+    //     direction = default;
+    //
+    //     return eventType switch
+    //     {
+    //         EventType.Swapped => (direction = quest.CompareSwaps(stats)) == 0,
+    //         EventType.Matched => (direction = quest.CompareMatches(stats)) == 0,
+    //         _ => false
+    //     };
+    // }
 
-        return eventType switch
-        {
-            EventType.Swapped => (direction = quest.CompareSwaps(allStats)) == 0,
-            EventType.Matched => (direction = quest.CompareMatches(allStats)) == 0,
-            _ => false
-        };
-    }
+    protected abstract void CompareQuest2State();
 
-    protected abstract void HandleEvent();
-    
     public static void ActivateHandlers()
     {
         MatchQuestHandler.Instance.Subscribe();
-        SwapQuestHandler.Instance.Subscribe();
     }
 
     //its private for now until I will need my other Handlers for Clicks and Destruction events, which has to be 
@@ -129,111 +115,59 @@ public abstract class QuestHandler
     }
 }
 
-public sealed class SwapQuestHandler : QuestHandler
-{
-    public static SwapQuestHandler Instance { get; } = GetInstance<SwapQuestHandler>();
-
-    private SwapQuestHandler() : base(typeof(SwapQuestHandler)) 
-        => Game.OnTileSwapped += HandleEvent;
-
-    protected override void HandleEvent()
-    {
-        //... needs logic!...//
-    }
-}
-
 public sealed class MatchQuestHandler : QuestHandler
 {
-    private void CompareFinalResults()
-    {
-        //clear whatever was written inside the logger, because we are done now anyways and we wanna store inside 
-        //to see what is the final GameState
-        
-        GameState.Logger.Clear();
-
-        switch (IsMainGoalReached)
-        {
-            case true when !GameState.IsGameOver:
-                GameState.WasGameWonB4Timeout = true;
-                GameState.Logger.Append("NICE, YOU FINISHED THE ENTIRE MATCH-QUEST BEFORE THE GAME ENDED! WELL DONE Man!");
-                break;
-
-            case false when GameState.IsGameOver:
-                GameState.WasGameWonB4Timeout = false;
-                GameState.Logger.Append(
-                    "Unlucky, very close, you will do it next time, i am sure :-), but here your results" +
-                    $"So far you have gotten at least {QuestCounter} done and you needed actually still {QuestLeft} more");
-                break;
-
-            case true when GameState.IsGameOver:
-                GameState.WasGameWonB4Timeout = false;
-                GameState.Logger.Append("Good job, you did in time at least, you piece of shit!!");
-                break;
-        }
-    }
+    public static readonly MatchQuestHandler Instance = GetInstance<MatchQuestHandler>();
 
     private MatchQuestHandler() : base(typeof(MatchQuestHandler))
     {
-        Game.OnMatchFound += HandleEvent;
+        Game.OnTileSwapped += CompareQuest2State;
+        Game.OnMatchFound += CompareQuest2State;
     }
 
-    public static MatchQuestHandler Instance { get; } = GetInstance<MatchQuestHandler>();
-    
-    private static bool IsMatchQuestReached(out Quest? quest, in AllStats allStats, out int direction)
+    protected override void CompareQuest2State()
     {
-        if (allStats.Matched is null)
+        var questRunner = QuestBuilder.GetQuests();
+
+        foreach (var quest in questRunner)
         {
-            quest = null;
-            direction = int.MaxValue;
-            return false;
-        }
+            int swaps2Do = quest.Swap!.Value.Count;
+            EventStats? currData = GameState.CurrentData;
+            float swapTimeAllowed = quest.Swap!.Value.Interval;
+            int matchQuest = quest.Match!.Value.Count;
+            
+            TileColor
+                x = currData!.TileX!.Body.TileColor,
+                y = currData.TileY!.Body.TileColor;
 
-        var type = GameState.Tile.Body.TileColor;
-        quest = QuestBuilder.GetQuestFrom(type);
-        return IsSubQuestReached(EventType.Matched, quest.Value, allStats, out direction);
-    }
-
-    protected override void HandleEvent()
-    {
-        var type = GameState.Matches!.Body!.TileColor;
-        ref var stats = ref Grid.GetStatsByType(type);
-
-        if (stats.Matched is null)
-        {
-            GameState.Matches.Clear();
-            return;
-        }
-
-        stats[EventType.Matched].Count++;
-
-        if (IsMatchQuestReached(out var goal, stats, out int compareResult))
-        {
-            QuestCounter++;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Sub goals done:  {QuestCounter}");
-            Console.ResetColor();
-            //Console.WriteLine($"yea, we got {stats.Matched!.Value.Count} match of type: {type} within");
-            //Console.WriteLine($"current match goal:  {goal?.Match}");
-            stats.Matched = null;
-            //Console.WriteLine("YEA YOU GOT current MATCH AND ARE REWARDED FOR IT !: ");
-            Console.Clear();
-            // Grid.Instance.Delete(GameState.Matches);
-        }
-        else
-        {
-            CompareFinalResults();
-
-            //error-code
-            if (compareResult > 1)
+            bool sameTileColor = (x == quest.TileColor || y == quest.TileColor);
+            //the more this if- is executed the worse, because it says that we dont get matches for each swap we do!
+            if (!currData.wasMatch || x != quest.TileColor || y != quest.TileColor)
             {
-                GameState.Matches.Clear();
+                Debug.WriteLine(
+                    $"A swap between <{currData.TileX!.Body.TileColor}> " +
+                    $"AND <{currData.TileY!.Body.TileColor}> was made" +
+                    $"and it did not lead to a match..");
+            }
+            //we have a match and also its the same color BUT we needed to long for the next swap
+            //and hence failed... 
+            else if (currData.wasMatch && sameTileColor
+                     && swapTimeAllowed >= currData.Interval)
+            {
+                Debug.WriteLine("Upsi, you needed to long amigo");
+            }
+            else if (currData.wasMatch && sameTileColor &&
+                     currData.Count == matchQuest)
+            {
+                Debug.WriteLine("ADD BONUS POINTS TO SCORE!");
             }
 
-            //Console.BackgroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"yea, we got {stats.Matched!.Value.Count} match of type: {type} within");
-            Console.WriteLine($"current match goal:  {goal?.Match}");
-            // Grid.Instance.Delete(GameState.Matches);
+
+            if (currData.Count == swaps2Do && !currData.wasMatch)
+            {
+                Debug.WriteLine("Now you gotta be punished, NOT 1 MATCH!! WTF");
+            }
         }
     }
 }
- 
+
