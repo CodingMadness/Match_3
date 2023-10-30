b94a6b0ed30fce43644121599a5cfcc6dd121e94
CodingMadness <shho310795@proton.me>
2023-10-30 12:32:25 +0100

    Added the "Level" field to have more cleaner  access

--- StateHolder/GameState.cs
+++ StateHolder/GameState.cs
@@ -1,10 +1,13 @@
-﻿namespace Match_3.StateHolder;
+﻿using Match_3.Service;
+
+namespace Match_3.StateHolder;
 
 public static class GameState
 {
     public static bool EnemiesStillPresent, WasGameWonB4Timeout, IsGameOver;
 
     public static Quest[]? Quests;
-    public static EventState? CurrentData;
+    public static Level? CurrentLvl;
+    public static EventState? CurrData;
     public static SpanQueue<char>? Logger; //whatever the logger logged, take that to render!
 }
