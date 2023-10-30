6b0125dccefd4d6c308f2d12dc55b9e6065e7d31
CodingMadness <shho310795@proton.me>
2023-10-31 00:49:27 +0100

    Added 2 important functions here: "GetQuests()" and "GetQuestsBy()" to encapsulate the internal Quests[] array bit further.

--- StateHolder/GameState.cs
+++ StateHolder/GameState.cs
@@ -1,4 +1,5 @@
-﻿using Match_3.Service;
+﻿using System.Collections;
+using Match_3.Service;
 
 namespace Match_3.StateHolder;
 
@@ -6,8 +7,25 @@ public static class GameState
 {
     public static bool EnemiesStillPresent, WasGameWonB4Timeout, IsGameOver;
 
+    public static int QuestCount;
     public static Quest[]? Quests;
     public static Level? CurrentLvl;
     public static EventState? CurrData;
     public static SpanQueue<char>? Logger; //whatever the logger logged, take that to render!
+    
+    public static FastSpanEnumerator<Quest> GetQuests()
+        => new(Quests.AsSpan().TrimEnd(default(Quest)));
+
+    public static ref readonly Quest GetQuestBy(TileColor tileColor)
+    {
+        var iterator = new FastSpanEnumerator<Quest>(Quests.AsSpan(0, QuestCount));
+
+        foreach (ref readonly var quest in iterator)
+        {
+            if (quest.TileColor == tileColor)
+                return ref quest;
+        }
+
+        return ref Quest.Empty;
+    }
 }
