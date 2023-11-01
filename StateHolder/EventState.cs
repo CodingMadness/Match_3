ad50f22985bf2a7c9686e9c466b770adcaab5c73
CodingMadness <shho310795@proton.me>
2023-11-01 05:50:49 +0100

    Removed some obsolete fields.

--- StateHolder/EventState.cs
+++ StateHolder/EventState.cs
@@ -1,14 +1,9 @@
-﻿using System.Numerics;
-
-namespace Match_3.StateHolder;
+﻿namespace Match_3.StateHolder;
 
 public class EventState
 {
-    public Vector2 Coords;
-    public float Interval;
-    public int Count;
     public Tile? TileX;
     public Tile? TileY;
-    public MatchX? Matches;
-    public bool WasMatch, StillTimeLeft, WasSwapped;
+    public MatchX? Matches = new();
+    public bool WasMatch, WasSwapped;
 }
