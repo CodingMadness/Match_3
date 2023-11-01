b70f830d37e9dd211c12b53b4266d377c0955d09
CodingMadness <shho310795@proton.me>
2023-11-01 05:53:01 +0100

    Added: a "State[] StatePerQuest" and a filtered Enumerable "InGameUsedTileTypes" to store only those "State" objects who are directly being used in "Swap()" method.
    Added a "GetStateBy()" method to get the proper "State" based on the given "TileColor"

--- StateHolder/GameState.cs
+++ StateHolder/GameState.cs
@@ -1,31 +1,44 @@
-﻿using System.Collections;
-using Match_3.Service;
+﻿using Match_3.Service;
 
 namespace Match_3.StateHolder;
 
 public static class GameState
 {
     public static bool EnemiesStillPresent, WasGameWonB4Timeout, IsGameOver;
-
     public static int QuestCount;
     public static Quest[]? Quests;
+    public static State[]? StatePerQuest;
+    public static IEnumerable<State>? InGameUsedTileTypes;
     public static Level? CurrentLvl;
     public static EventState? CurrData;
     public static SpanQueue<char>? Logger; //whatever the logger logged, take that to render!
     
     public static FastSpanEnumerator<Quest> GetQuests()
-        => new(Quests.AsSpan().TrimEnd(default(Quest)));
+        => new(Quests.AsSpan(0, QuestCount));
 
     public static ref readonly Quest GetQuestBy(TileColor tileColor)
     {
         var iterator = new FastSpanEnumerator<Quest>(Quests.AsSpan(0, QuestCount));
 
-        foreach (ref readonly var quest in iterator)
+        foreach (ref readonly Quest quest in iterator)
         {
             if (quest.TileColor == tileColor)
                 return ref quest;
         }
 
         return ref Quest.Empty;
+    }
+    
+    public static State GetStateBy(TileColor tileColor)
+    {
+        var iterator = new FastSpanEnumerator<State>(StatePerQuest.AsSpan(0, QuestCount));
+
+        foreach (State state in iterator)
+        {
+            if (state.TileColor == tileColor)
+                return state;
+        }
+
+        return null!;
     }
 }
