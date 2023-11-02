58accb5957a2cb3ba96423ce35702ae3892c5c97
CodingMadness <shho310795@proton.me>
2023-11-02 06:11:26 +0100

    Changed "Elapsed" from "TimeSpan" to "TimeOnly", because when I actually wanna compute the difference from event happening til comparison of "State" and "Quest" I can compute the timespan myself, I only need "begin" and "Now/End" to get the difference. Also renamed all the Record-fields to something more reasonable.

--- DataObjects/EventData.cs
+++ DataObjects/EventData.cs
@@ -1,6 +1,6 @@
 ﻿using DotNext;
 
-namespace Match_3.StateHolder;
+namespace Match_3.DataObjects;
 
 public record SubEventData(int Count, float Elapsed)
 {
@@ -9,44 +9,44 @@ public record SubEventData(int Count, float Elapsed)
 }
 
 public readonly record struct Quest(
-    TileColor TileColor,
+    TileColor TileKind,
     GameTime Timer,
-    SubEventData Match,
-    SubEventData Swap,
-    SubEventData Replacement,
-    SubEventData MissMatch)
+    SubEventData SuccessfulMatches,
+    SubEventData SwapsAllowed,
+    SubEventData ReplacementsAllowed,
+    SubEventData MissMatchesAllowed)
 {
-    public static readonly Quest Empty = default!;
-    public const string MatchCountName    = nameof(State.Match) + "." + nameof(State.Match.Count);
-    public const string MatchIntervalName = nameof(State.Match) + "." + nameof(State.Match.Elapsed);
-    public const string SwapCountName     = nameof(State.Swap) + "." + nameof(State.Swap.Count);
+    public static readonly Quest Empty       = default!;
+    public const string MatchCountName       = nameof(State.SuccessfulMatch) + "." + nameof(State.SuccessfulMatch.Count);
+    public const string MatchIntervalName    = nameof(State.SuccessfulMatch) + "." + nameof(State.SuccessfulMatch.Elapsed);
+    public const string SwapCountName        = nameof(State.Swap) + "." + nameof(State.Swap.Count);
     public const string ReplacementCountName = nameof(State.Replacement) + "." + nameof(State.Replacement.Count);
-    public const string MissMatchName     = nameof(State.MissMatch) + "." + nameof(State.MissMatch.Count);
+    public const string MissMatchName        = nameof(State.MissMatch) + "." + nameof(State.MissMatch.Count);
     
     public int GetValueByMemberName(ReadOnlySpan<char> name)
     {
         //FOR NOW we compare the contents but only until I have written a replace method to work merely on 
         // Spans and does not create a new copy!!!
         if (name.BitwiseEquals(MatchCountName))
-            return Match.Count;
+            return SuccessfulMatches.Count;
         else if (name.BitwiseEquals(MatchIntervalName))
-            return (int)Match.Elapsed;
+            return (int)SuccessfulMatches.Elapsed;
         else if (name.BitwiseEquals(SwapCountName))
-            return Swap.Count;
+            return SwapsAllowed.Count;
         else if (name.BitwiseEquals(ReplacementCountName))
-            return Replacement.Count;
+            return ReplacementsAllowed.Count;
         else if (name.BitwiseEquals(MissMatchName))
-            return MissMatch.Count;
+            return MissMatchesAllowed.Count;
 
         throw new ArgumentException("this code should not be reached at all!");
     }
 }
 
 public record State(
-    TileColor TileColor,
+    TileColor TileKind,
     bool IsQuestLost,
-    TimeSpan Elapsed,
-    SubEventData Match,
+    TimeOnly Now,
+    SubEventData SuccessfulMatch,
     SubEventData Swap,
     SubEventData Replacement,
     SubEventData MissMatch)
