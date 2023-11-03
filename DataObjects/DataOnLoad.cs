44e9f9316b78a722f816e9874128ad9c99a37a86
CodingMadness <shho310795@proton.me>
2023-11-03 06:17:30 +0100

    Renamed "Level" to "DataOnLoad" to refer to how its used, that it is only filled with data at some init-point and then never touched again, but its still not readonly, will check that in the future.

---
+++ DataObjects/DataOnLoad.cs
@@ -0,0 +1,24 @@
+﻿namespace Match_3.DataObjects;
+
+/// <summary>
+/// This Record defines data which has to be known BEFORE the actual Game runs!
+/// </summary>
+/// <param name="Id">The current levelID, like 0, 1, 2: the higher the number the harder the level gets</param>
+/// <param name="GameBeginAt">The startup time of the Game</param>
+/// <param name="GameOverScreenCountdown">The countdown which runs to 0, and so long you see a "GameOverScreen"</param>
+/// <param name="GridWidth">an integer number defining the count of tiles PER WIDTH</param>
+/// <param name="GridHeight">an integer number defining the count of tiles PER HEIGHT</param>
+public record DataOnLoad(int Id, 
+    int GameBeginAt, int GameOverScreenCountdown,
+    int GridWidth, int GridHeight)
+{
+    public const int MaxTilesPerMatch = 3;
+    public const int TileSize = 64 / 1;
+    public int WindowHeight => GridHeight * TileSize;
+    public int WindowWidth => GridWidth * TileSize;
+
+    public Quest[] Quests;
+
+    public int QuestCount;
+    public BitPack64 PackedCounts;
+}
