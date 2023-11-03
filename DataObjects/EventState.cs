d4a15d59d45d8aae9d21d16033b3555be1a9f87d
CodingMadness <shho310795@proton.me>
2023-11-03 06:23:11 +0100

    Splitted all the nessecary data from all classes into "EventState" when it has to do with data being used and stored when a cruicial event is happening.

--- DataObjects/EventState.cs
+++ DataObjects/EventState.cs
@@ -2,9 +2,15 @@
 
 public class EventState
 {
+    public State[]? StatePerQuest;
+    public IEnumerable<State>? StatesFromQuestRelatedTiles;
     public TileColor IgnoredByMatch;
     public Tile? TileX;
     public Tile? TileY;
     public MatchX? Matches = new();
-    public bool WasMatch, WasSwapped;
+    public bool WasMatch, 
+                WasSwapped,
+                EnemiesStillPresent,
+                WasGameWonB4Timeout,
+                IsGameOver;
 }
