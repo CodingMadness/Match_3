29efae1f3dca19254c859ea514c5d4daf9d279e9
CodingMadness <shho310795@proton.me>
2023-11-06 10:21:13 +0100

    Significantly debloated the "Comparer" classes since alot of these cheks do not have to happen on every comparison and hence got removed, now they are only done in the specific areas of need.
    Renamed and reworkd "StateAndBodyComparer" to only "BodyComparer" since we only want to compare its body and the state was more or less only needed as another distinction.
    
    Added a new Comparer: "DistanceComparer" which derives from "CellComparer" because both are in base the same but with the addition that as the name already suggests, he also compares by "Distance" not just by cell.

--- Service/Comparer.cs
+++ Service/Comparer.cs
@@ -1,48 +1,110 @@
-﻿using Match_3.DataObjects;
+﻿using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Numerics;
+using System.Runtime.Intrinsics;
+using DotNext.Runtime;
+using Match_3.DataObjects;
 
 namespace Match_3.Service;
 
 public static class Comparer
 {
-    public sealed class StateAndBodyComparer : EqualityComparer<Tile>
+    public sealed class BodyComparer : EqualityComparer<Tile>
     {
+        /// <summary>
+        /// Since I derive this class from "EqualityComparer", I HAVE TO take Tile? params
+        /// but I make sure that they NEVER can be null, it is illogical for that to ever happen
+        /// and I make sure that I check that in the specific sections of code where needed but not here
+        /// since it is not needed for every Equals() call and would waste senselessly performance
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public override bool Equals(Tile? x, Tile? y)
         {
-            if (x is not { }) return false;
-            if (y is not { }) return false;
-            if (ReferenceEquals(x, y)) return true;
-            if (ReferenceEquals(x, null)) return false;
-            if (ReferenceEquals(y, null)) return false;
-            if (x.GetType() != y.GetType()) return false;
-            if ((x.TileState & TileState.Deleted) == TileState.Deleted ||
-                (x.TileState & TileState.Disabled) == TileState.Disabled) return false;
-        
-            return x.Body.Equals(y.Body);
+            return ReferenceEquals(x, y) || x!.Body.Equals(y!.Body);
         }
+
         public override int GetHashCode(Tile obj)
         {
-            return HashCode.Combine((int)obj.TileState, obj.Body);
+            return HashCode.Combine(obj.Body);
         }
-        public static StateAndBodyComparer Singleton => new();
+
+        public static readonly BodyComparer Singleton = new();
     }
 
-    public sealed class CellComparer : EqualityComparer<Tile>, IComparer<Tile>
+    public class CellComparer : EqualityComparer<Tile>, IComparer<Tile>
     {
+        public static readonly CellComparer Singleton = new();
+
+        protected internal CellComparer()
+        {}
+
+        [Pure]
         public override bool Equals(Tile? x, Tile? y)
-        {
-            return Compare(x, y) == 0;
-        }
+            => ReferenceEquals(x, y) || x!.GridCell == y!.GridCell;
+
+        [Pure]
         public override int GetHashCode(Tile obj)
+            => obj.GridCell.GetHashCode();
+ 
+        [Pure]
+        public virtual int Compare(Tile? x, Tile? y)
         {
-            return obj.GridCell.GetHashCode();
+            if (Equals(x, y))
+                return 0;
+            
+            Intrinsics.Bitcast(x!.GridCell, out (float, float) tuple);
+            Intrinsics.Bitcast(y!.GridCell, out (float, float) tuple2);
+            
+            return tuple.CompareTo(tuple2);
         }
-        public int Compare(Tile? a, Tile? b)
+    }
+
+    public class DistanceComparer : CellComparer
+    {
+        private readonly float _toleratedDistance;
+        
+        public DistanceComparer(float toleratedDistance = 3.5f)
         {
-            if (a is null) return -1;
-            if (a == b) return 0;
-            if (b is null) return 1;
-            return a.GridCell.CompareTo(b.GridCell);
+            _toleratedDistance = toleratedDistance;
         }
-        public static CellComparer Singleton => new();
+        
+        public override int Compare(Tile? x, Tile? y)
+        {
+            if (base.Equals(x, y))
+                return 0;
+            
+            float distance = Vector2.Distance(x!.GridCell, y!.GridCell);
+            int result;
+            
+            //to close! they cluster...
+            if (distance <= _toleratedDistance)
+            {
+                result = -1;
+                // Console.WriteLine($"Is {x!.GridCell} to close to {y!.GridCell}?   Range is: {distance}");
+                return result;
+            }
+
+            result = 1;
+            return result;
+        }
+        
+        public bool? Are2Close(Tile? a, Tile? b)
+        {
+            //-1 => TOP/LEFT;
+            // 1 => BOT/RIGHT
+            bool? res = Compare(a, b) switch
+            {
+               -1 => true,   /*-1 means that the distance between x and y is < 3f (_tolerance) and hence to close!
+                                so they will be sorted to the top of the collection!*/
+                               
+                1 => false,  /* 1 means its > 3f, so its enough space, so they will be sorted to the bottom of the collection!*/
+               
+                0 => null,
+            };
+            return res;
+        }
     }
 }
