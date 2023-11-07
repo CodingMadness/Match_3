5bcfc52c7a08065402027b01a6b7345eb08872b1
CodingMadness <shho310795@proton.me>
2023-11-07 18:13:30 +0100

    Added "OrderByColumns/OrderByRows" logic to distinct between 2 possible ways in how to compare/order tiles in the grid.

--- Service/Comparer.cs
+++ Service/Comparer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics.Contracts;
 using System.Numerics;
 using DotNext.Runtime;
-
 using Match_3.DataObjects;
 
 namespace Match_3.Service;
@@ -34,10 +33,13 @@ public static class Comparer
 
     public class CellComparer : EqualityComparer<Tile>, IComparer<Tile>
     {
+        private readonly bool _orderByColumns;
         public static readonly CellComparer Singleton = new();
 
-        protected CellComparer()
-        {}
+        protected CellComparer(bool orderByColumns = true)
+        {
+            _orderByColumns = orderByColumns;
+        }
 
         [Pure]
         public override bool Equals(Tile? x, Tile? y)
@@ -46,38 +48,55 @@ public static class Comparer
         [Pure]
         public override int GetHashCode(Tile obj)
             => obj.GridCell.GetHashCode();
- 
+
         [Pure]
         public virtual int Compare(Tile? x, Tile? y)
         {
             if (Equals(x, y))
                 return 0;
-            
-            Intrinsics.Bitcast(x!.GridCell, out (float x, float y) tuple);
-            Intrinsics.Bitcast(y!.GridCell, out (float x, float y) tuple2);
 
             //based on Grid logic, there are only integer cells, but since using 
             //Numerics.Vector2, I have to cast them from float to int,
             //because they are by default floats
-            //So, when x is either < or > other x
+            Intrinsics.Bitcast(x!.GridCell, out (int x0, int y0) tuple0);
+            Intrinsics.Bitcast(y!.GridCell, out (int x1, int y1) tuple1);
+
+            //So, when "_orderByColumns" is true, we first consider "x" only if x1==x2 then 
             //then we only do care for those x values because it
             //doesnt matter if y is different, x is sufficient then
             //but IF x1==x2 THEN we care for y and we check for that, 
-            return (int)tuple.x != (int)tuple2.x
-                ? tuple.x.CompareTo(tuple2.x) 
-                : tuple.y.CompareTo(tuple2.y);
+            int result;
+            bool orderByRows = !_orderByColumns;
+
+            if (_orderByColumns)
+            {
+                if ((result=tuple0.x0.CompareTo(tuple1.x1)) == 0)
+                    return tuple0.y0.CompareTo(tuple1.y1);
+                else
+                    return result;
+            }
+            //we have to order first by rows!
+            if (orderByRows)
+            {
+                if ((result=tuple0.y0.CompareTo(tuple1.y1)) == 0)
+                    return tuple0.x0.CompareTo(tuple1.x1);
+                else
+                    return result;
+            }
+
+            throw new ArgumentException($"something went heavily wrong inside: CellComparer.CompareTo(x, y)...");
         }
     }
 
-    public class DistanceComparer : CellComparer
+    public sealed class DistanceComparer : CellComparer
     {
         private readonly float _toleratedDistance;
         
-        public DistanceComparer(float toleratedDistance = 3.5f) 
+        public DistanceComparer(float toleratedDistance = 3.5f)
         {
             _toleratedDistance = toleratedDistance;
         }
-        
+
         public override int Compare(Tile? x, Tile? y)
         {
             if (base.Equals(x, y))
@@ -85,30 +104,33 @@ public static class Comparer
             
             float distance = Vector2.Distance(x!.GridCell, y!.GridCell);
             int result;
-            
+
             //to close! they cluster...
             if (distance <= _toleratedDistance)
             {
                 result = -1;
                 // Console.WriteLine($"Is {x!.GridCell} to close to {y!.GridCell}?   Range is: {distance}");
+                // x.NodeCount++;
+                //y.NodeCount++;
                 return result;
             }
 
             result = 1;
+            
             return result;
         }
-        
-        public bool? Are2Close(Tile? a, Tile? b)
+
+        public bool? Are2Close(Tile? x, Tile? y)
         {
             //-1 => TOP/LEFT;
             // 1 => BOT/RIGHT
-            bool? res = Compare(a, b) switch
+            bool? res = Compare(x, y) switch
             {
-               -1 => true,   /*-1 means that the distance between x and y is < 3f (_tolerance) and hence to close!
-                                so they will be sorted to the top of the collection!*/
-                               
-                1 => false,  /* 1 means its > 3f, so its enough space, so they will be sorted to the bottom of the collection!*/
-               
+                -1 => true, /*-1 means that the distance between x and y is < 3f (_tolerance) and hence to close!
+                                 so they will be sorted to the top of the collection!*/
+
+                1 => false, /* 1 means its > 3f, so its enough space, so they will be sorted to the bottom of the collection!*/
+
                 0 => null,
             };
             return res;
