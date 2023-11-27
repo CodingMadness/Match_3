f82aaf30063e72d24274d4379dcdfc3b7588f780
CodingMadness <shho310795@proton.me>
2023-11-27 05:14:40 +0100

    Reworked the entire former "Scale" struct to be fully readonly now

--- DataObjects/Scale.cs
+++ DataObjects/Scale.cs
@@ -1,35 +1,72 @@
-﻿using Match_3.Service;
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
+using Match_3.Service;
+using Raylib_cs;
+using Rectangle = System.Drawing.Rectangle;
 
 namespace Match_3.DataObjects;
 
-public struct Scale(float minScale, float maxScale)
+
+///We initialize a "Scale" by:
+///  * min
+///  * max
+///  * speed
+///  * ElapsedTime=1f
+public readonly struct Scale(float start, float speed=10f, float min=-1f, float max=1f, float elapsedTime=1f)
 {
-    private float _direction = -1f;
-    private float _finalScaleFactor = minScale.Equals(maxScale, 0.1f) ? minScale : 1f;
-
-    public float Speed = 0f;
-    public float ElapsedTime;
-
-    public float GetFactor()
+    private readonly float Result = start;
+    
+    public Scale GetFactor()
     {
-        if (ElapsedTime <= 0f)
-            return _finalScaleFactor;
+        float sizeDirection = 1f;
+        float factor = start;
+
+        if (elapsedTime <= 0f)
+            return this;
         
-        if (_finalScaleFactor.Equals(minScale, 0.1f) || 
-            _finalScaleFactor.Equals(maxScale, 0.1f))
+        //this means we reached the respective end (either in - or + area) 
+        if (factor.Equals(min, 0.1f) || factor.Equals(max, 0.1f))
         {
             //so we start at scale1: then it scaled slowly down to "_minScale" and then from there
             //we change the multiplier to now ADD the x to the scale, so we scale back UP
             //this created this scaling flow
-            _direction *= -1;  
+            sizeDirection *= -1;  
         }
-        float x = Speed * (1 / ElapsedTime); 
-        return _finalScaleFactor += _direction * x;
+        
+        float x = speed * (1f / elapsedTime);
+        factor += (sizeDirection * x);
+
+        return new(elapsedTime: elapsedTime, start: factor);
+    }
+
+    void Test(float seconds)
+    {
+        Scale up = new(start: 0f, elapsedTime: seconds);
     }
     
-    public static implicit operator Scale(float size) => new(size, size)
+    public static (CSharpRect newBox, Scale next) operator *(Scale scale, RectangleF cSharpRect)
     {
-        ElapsedTime = 0f,
-        //_direction = 0,
-    };
+        (CSharpRect newBox, Scale next) = (default, scale.GetFactor());
+        newBox.Width = cSharpRect.Width * next.Result;
+        newBox.Height = cSharpRect.Height * next.Result;
+        return (newBox, next);
+    }
+
+    public static (RayRect newBox, Scale next) operator *(Scale scale, RayRect rayRect)
+    {
+        (RayRect newBox, Scale next) = (default, scale.GetFactor());
+        newBox.width = rayRect.width * next.Result;
+        newBox.height = rayRect.height * next.Result;
+        return (newBox, next);
+    }
+
+    public static (CSharpRect newBox, Scale next) operator *(Scale scale, SizeF size)
+    {
+        (CSharpRect newBox, var next) = (default, scale.GetFactor());
+        newBox.Width = size.Width * next.Result;
+        newBox.Height = size.Height * next.Result;
+        return (newBox, next);
+    }
+
+    public override string ToString() => $"scaling by: <{Result}>";
 }
