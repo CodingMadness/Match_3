0058c9af783837dff3903784d27b2df3878a5d17
CodeBro <28008874+CodingMadness@users.noreply.github.com>
2025-05-27 03:52:04 +0200

    Important change to the "QuestLog" constant; Now has added more empty chars in order to avoid runtime exceptions when a tilecolor is actually longer than the literal word "TileColor"

--- DataObjects/GameState.cs
+++ DataObjects/GameState.cs
@@ -1,4 +1,5 @@
-﻿using Match_3.Service;
+﻿using JetBrains.Annotations;
+using Match_3.Service;
 
 namespace Match_3.DataObjects;
 
@@ -19,24 +20,27 @@ public sealed class GameState
     public Config Lvl { get; set; }  // Set this during game initialization
 
     // QuestLog remains a computed string (now with instance access)
-    public const string QuestLog = $"(Black) You have to collect ({Quest.TileColorName}) {Quest.MatchCountName} Matches " +
-                             $"(Black) and u have in between those only ({Quest.TileColorName}) {Quest.MatchIntervalName} seconds left " +
-                             $"(Black) and also just ({Quest.TileColorName}) {Quest.SwapCountName} swaps available per match " +
-                             $"(Black) and furthermore, you only are allowed to replace any given tile ({Quest.TileColorName}) {Quest.ReplacementCountName} times maximum " +
-                             $"(Black) for your own help as well as there is only tolerance for ({Quest.TileColorName}) {Quest.WrongMatchName} wrong matches";
+    public const string QuestLog = $"(Black) You have to collect ({Quest.TileColorName}   ) {Quest.MatchCountName} Matches " +
+                             $"(Black) and u have in between those only ({Quest.TileColorName}   ) {Quest.MatchIntervalName} seconds left " +
+                             $"(Black) and also just ({Quest.TileColorName}   ) {Quest.SwapCountName} swaps available per match " +
+                             $"(Black) and furthermore, you only are allowed to replace any given tile ({Quest.TileColorName}   ) {Quest.ReplacementCountName} times maximum " +
+                             $"(Black) for your own help as well as there is only tolerance for ({Quest.TileColorName}   ) {Quest.WrongMatchName} wrong matches";
 
     // --- Lazy-Loaded Resources ---
     private SpanQueue<char>? _logger;
 
     public SpanQueue<char> Logger => _logger ??= new SpanQueue<char>(QuestLog.Length * Lvl.QuestCount);
 
+    [Pure]
     public ReadOnlySpan<char> GetPooledQuestLog() => Logger.Dequeue(true);
 
     // --- Current State ---
     public readonly EventState CurrData = new();  
 
+    [Pure]
     public Span<Quest> GetQuests() => Lvl.Quests.AsSpan(0, Lvl.QuestCount);
 
+    [Pure]
     public ref readonly Quest GetQuestBy(TileColor tileColor)
     {
         var onlyNeededQuests = Lvl.Quests.AsSpan(0, Lvl.QuestCount);
@@ -50,6 +54,7 @@ public sealed class GameState
         return ref Quest.Empty;
     }
     
+    [Pure]
     public State GetStateBy(TileColor tileColor)
     {
         var states = CurrData.StatePerQuest.AsSpan(0, Lvl.QuestCount);
