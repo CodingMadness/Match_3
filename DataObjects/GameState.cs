f32350241fabd6193af5883ffcc44e1821e86e40
CodeBro <28008874+CodingMadness@users.noreply.github.com>
2025-06-01 02:27:07 +0200

    Updated "GameState" to now hold ALL nessecary game relevant data and removed obsolete code

--- DataObjects/GameState.cs
+++ DataObjects/GameState.cs
@@ -1,20 +1,12 @@
-﻿using JetBrains.Annotations;
+﻿namespace Match_3.DataObjects;
 
-namespace Match_3.DataObjects;
-
-public class QuestState(
-    TileColorTypes ColourType,
-    bool IsQuestLost,
-    (int Count, float Elapsed) _foundMatch,
-    (int Count, float Elapsed) _wrongSwaps,
-    (int Count, float Elapsed) _replacementsUsed,
-    (int Count, float Elapsed) _wrongMatch)
+public class QuestState(TileColorTypes ColourType)
 {
-    public (int Count, float Elapsed) FoundMatch { get; set;} = _foundMatch;
-    public (int Count, float Elapsed) WrongSwaps { get; set;} = _wrongSwaps;
-    public (int Count, float Elapsed) ReplacementsUsed { get; set;} = _replacementsUsed;
-    public (int Count, float Elapsed) WrongMatch { get; set;} = _wrongMatch;
-    public bool IsQuestLost { get; set; } = IsQuestLost;
+    public (int Count, float Elapsed) FoundMatch { get; set; }
+    public (int Count, float Elapsed) WrongSwaps { get; set; }
+    public (int Count, float Elapsed) ReplacementsUsed { get; set; }
+    public (int Count, float Elapsed) WrongMatch { get; set; }
+    public bool IsQuestLost { get; set; }
     public Tile? Current { get; set; }
     public TileColorTypes ColourType { get; } = ColourType;
 }
@@ -24,15 +16,26 @@ public sealed class GameState
     // Singleton instance (thread-safe & lazy)
     private static readonly Lazy<GameState> _instance = new(() => new GameState());
 
-    public readonly MatchX Matches = [];
-    private int _questCount;
+    public MatchX Matches { get; } = [];
 
-    public QuestState[] States { get; private set; }
-    public QuestHolder Holder { get; set; }
+    //make this be loaded only once on a custom method()!
+    public QuestState[] States { get; set; } = null!;
+    public Quest[] ToAccomplish { get; set; } = null!;
+    public QuestLogger Logger { get; set; } = null!;
+
+    public int CurrentQuestCount
+    {
+        get => field is 0 ? ToAccomplish.Length : field;
+        set;
+    }
+
+    public bool IsInGame { get; set; }
+    public GameTime GetCurrentTime(in Config config) => GameTime.CreateTimer(config.GameBeginAt);
     public bool WasGameLost { get; set; }
     public bool WasGameWon { get; set; }
     public bool HaveAMatch { get; set; }
     public bool WasSwapped { get; set; }
+
     public int LevelId { get; set; }
     public Tile? TileY; //they must be fields, because I need later them to be used via "ref" directly!
     public Tile? TileX; //they must be fields, because I need later them to be used via "ref" directly!
@@ -43,22 +46,6 @@ public sealed class GameState
 
     public Direction LookUpUsedInMatchFinder { get; set; }
 
-    public void InitStates(int questCount)
-    {
-        _questCount = questCount;
-        States = new QuestState[questCount];
-    }
-
-    public void DefineStateType(int index, TileColorTypes colorType)
-    {
-        States[index] = new(colorType,
-            false,
-            (0, 0),
-            (0, 0),
-            (0, 0),
-            (0, 0));
-    }
-
     public static GameState Instance => _instance.Value;
 
     // Private constructor to prevent external instantiation
@@ -66,18 +53,10 @@ public sealed class GameState
     {
         // Initialize QuestLogger only when first needed (lazy via property)
     }
-
-    [Pure]
-    public QuestState GetStateBy(TileColorTypes tileColorTypes)
-    {
-        var states = States.AsSpan(0, _questCount);
-
-        foreach (var state in states)
-        {
-            if (state.ColourType == tileColorTypes)
-                return state;
-        }
-
-        return null!;
-    }
 }
+
+
+///Game -> GameState(Has ongoing state-change)
+///        |-------->QuestHolder (only init at start up, never changed after that!)
+///        |-------->QuestState  (init at start up, but changes possible after that! unlimited times)
+///        |-------->QuestLogger (init at start up, but changes possible after that! unlimited times)
