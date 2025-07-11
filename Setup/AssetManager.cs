9d1deb0c9db3cc79b4f104ca81fbb9141f450ae3
shpend <shho310795@proton.me>
2025-07-11 13:47:37 +0200

    Added Logic to one-time-init the needed AssetTypes

--- Setup/AssetManager.cs
+++ Setup/AssetManager.cs
@@ -7,6 +7,7 @@ using DotNext.Buffers;
 using DotNext.Runtime;
 using Match_3.DataObjects;
 using NetFabric.Hyperlinq;
+using Raylib_cs;
 
 namespace Match_3.Setup;
 
@@ -14,10 +15,9 @@ public class AssetManager
 {
     private static readonly AssetManager _instance = new();
     private readonly Assembly EmbeddedResources = Assembly.GetExecutingAssembly();
-    private Lazy<IEnumerable<string>> AllFilePaths => new(() => EmbeddedResources.GetManifestResourceNames(),
-        LazyThreadSafetyMode.ExecutionAndPublication);
+    private readonly Lazy<IEnumerable<string>> AllFilePaths;
     private readonly List<AssetFile> _allFiles = [];
-
+    
     private MemoryOwner<byte> GetEmbeddedResourceAsBytes(string fullPath)
     {
         using var stream = EmbeddedResources.GetManifestResourceStream(fullPath) ??
@@ -27,29 +27,33 @@ public class AssetManager
         return owner; // Caller must dispose
     }
 
+    private void GetFile(string fileName, out AssetFile assetFile)
+    {
+        var span = CollectionsMarshal.AsSpan(_allFiles);
+        var result = span.FirstOrNone(file => file.Name.BitwiseEquals(fileName));
+        assetFile = result.ValueRef;
+    }
+    
     private AssetManager()
     {
+        AllFilePaths = new(
+            () => EmbeddedResources.GetManifestResourceNames(), LazyThreadSafetyMode.ExecutionAndPublication);
     }
 
     public static readonly AssetManager Instance = _instance;
 
-    public void GetFile(string fileName, out AssetFile assetFile)
-    {
-        var span = CollectionsMarshal.AsSpan(_allFiles);
-        var result = span.FirstOrNone(file => file.FileInfo.Name.BitwiseEquals(fileName));
-        assetFile = result.ValueRef;
-    }
+    public Texture2D MySprite { get; private set; }
 
     public void LoadAssetFolder()
     {
         foreach (var filePath in AllFilePaths.Value)
         { 
             using var buffer = GetEmbeddedResourceAsBytes(filePath);
-            var file = new AssetFile(new(filePath), buffer.Span);
+            var file = new AssetFile(filePath, buffer.Span);
             _allFiles.Add(file);
         }
-
-        // var fileData2 = GetFile("set1.png");
-        //File.WriteAllBytes(@"C:\users\maho3\desktop\mySet.png", fileData2.Content);
+        
+        GetFile("set1.png", out  var assetFile);
+        MySprite = assetFile.Format.AsT1;
     }
 }
